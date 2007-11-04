c0e51ac1d6a14edcdf5f2fed0c07249aeeab7a6e
Shawn M Moore <sartak@gmail.com>
2007-11-04 05:51:45 -0500

    Add a TAEB::VT object for adding new methods

---
+++ lib/TAEB/VT.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+package TAEB::VT;
+use Moose;
+extends 'Term::VT102::ZeroBased';
+
+=head2 find_row CODE
+
+This is used to iterate over the virtual terminal's rows, looking for something.
+The callback receives the contents of each row in turn.
+
+If the callback returns a true value, then the find_row method will return
+the current row's index.
+
+If the callback returns all false values, then the find_row method will
+return C<undef>.
+
+=cut
+
+sub find_row {
+    my $self = shift;
+    my $cb = shift;
+
+    for my $row (0 .. $self->rows - 1) {
+        return $row if $cb->($self->row_plaintext($row));
+    }
+
+    return;
+}
+
+=head2 contains Str -> Bool
+
+Returns whether the specified string is contained in the virtual terminal's
+contents.
+
+=cut
+
+sub contains {
+    my $self = shift;
+    my $text = shift;
+
+    defined $self->find_row(sub { index($_[0], $text) >= 0 });
+}
+
+=head2 matches Regexp -> Bool
+
+Returns whether the specified regex matches any of the VT's rows.
+
+=cut
+
+sub matches {
+    my $self = shift;
+    my $re = shift;
+
+    defined $self->find_row(sub { $_[0] =~ $re });
+}
+
+1;
+
