72825d81679e5a37518f18632eca99a057c1bbe6
Shawn M Moore <sartak@gmail.com>
2007-11-04 04:16:42 -0500

    Flesh out what the primary TAEB module is for - and get rid of its CPAN doc

--- lib/TAEB.pm
+++ lib/TAEB.pm
@@ -1,17 +1,11 @@
 #!perl
 package TAEB;
 use Moose;
-
-has interface =>
-(
-    is       => 'rw',
-    isa      => 'TAEB::Interface',
-    required => 1,
-);
+use Term::VT102::ZeroBased;
 
 =head1 NAME
 
-TAEB - ???
+TAEB - Tactical Amulet Extraction Bot
 
 =head1 VERSION
 
@@ -21,67 +15,55 @@ Version 0.01 released ???
 
 our $VERSION = '0.01';
 
-=head1 SYNOPSIS
+has interface =>
+(
+    is       => 'rw',
+    isa      => 'TAEB::Interface',
+    required => 1,
+);
 
-    use TAEB;
-    do_stuff();
+has vt =>
+(
+    is => 'rw',
+    isa => 'Term::VT102::ZeroBased',
+    required => 1,
+    default => sub {
+        Term::VT102::ZeroBased->new(cols => 80, rows => 24)
+    },
+);
 
-=head1 DESCRIPTION
+=head2 step
 
+This will perform one input/output iteration of TAEB.
 
-
-=head1 SEE ALSO
-
-L<Foo::Bar>
-
-=head1 AUTHOR
-
-Shawn M Moore, C<< <sartak at gmail.com> >>
-
-=head1 BUGS
-
-No known bugs.
-
-Please report any bugs through RT: email
-C<bug-taeb at rt.cpan.org>, or browse to
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=TAEB>.
-
-=head1 SUPPORT
-
-You can find this documentation for this module with the perldoc command.
-
-    perldoc TAEB
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/TAEB>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/TAEB>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=TAEB>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/TAEB>
-
-=back
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2007 Shawn M Moore.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+It will return any input it receives, so you can follow along at home.
 
 =cut
+
+sub step {
+    my $self = shift;
+    my $input = $self->process_input;
+
+    return $input;
+}
+
+=head2 process_input
+
+This will read the interface for input and update the VT object.
+
+It will return any input it receives, so C<step> can follow along at home.
+
+=cut
+
+sub process_input {
+    my $self = shift;
+
+    my $input = $self->interface->read;
+
+    $self->vt->process($input);
+
+    return $input;
+}
 
 1;
 
