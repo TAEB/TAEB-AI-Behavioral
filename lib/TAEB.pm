58420d210ead00c971127da8537dff0837cbdd95
Shawn M Moore <sartak@gmail.com>
2007-11-04 02:57:38 -0500

    Initial import of TAEB

---
+++ lib/TAEB.pm
@@ -0,0 +1,83 @@
+#!perl
+package TAEB;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+TAEB - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use TAEB;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-taeb at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=TAEB>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc TAEB
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/TAEB>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/TAEB>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=TAEB>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/TAEB>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
---
+++ t/001-basic.t
@@ -0,0 +1,8 @@
+#!perl -T
+use strict;
+use warnings;
+use Test::More tests => 1;
+use TAEB;
+
+ok(1);
+
