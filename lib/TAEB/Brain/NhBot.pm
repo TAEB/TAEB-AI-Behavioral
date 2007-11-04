b6c62738d6a0c49b2b8225b26bd0fd3dbf8cc29b
Shawn M Moore <sartak@gmail.com>
2007-11-04 07:45:52 -0500

    Steal nhbot's "halp I'm being attacked" regex

--- lib/TAEB/Brain/NhBot.pm
+++ lib/TAEB/Brain/NhBot.pm
@@ -58,7 +58,7 @@ sub next_action {
         return "#pray\n";
     }
     # under attack! start responding
-    elsif ($taeb->vt->topline =~ /^The (\S+).* bites!/) {
+    elsif ($taeb->vt->topline =~ /(?:(?:the |an? )([-.a-z ]+?)|it) (?:just )?(strikes|hits|misses|bites|grabs|stings|touches|points at you, then curses)(?:(?: at)? you(?:r displaced image)?)?[.!]/) {
         $self->last_direction(-1);
         $self->looking_for($1);
         return $self->spin;
