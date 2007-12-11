dffb54984f04ce811a2b7e916ad7ba4ef9d70f49
Shawn M Moore <sartak@gmail.com>
2007-12-11 02:08:34 -0500

    Instantiate the current level under dungeons

--- lib/TAEB/World/Dungeon.pm
+++ lib/TAEB/World/Dungeon.pm
@@ -20,5 +20,15 @@ has current_level => (
     isa => 'TAEB::World::Level',
 );
 
+# we start off in dungeon 1. this helps keeps things tidy (we only have to
+# worry about level generation on level change)
+sub BUILD {
+    my $self = shift;
+    my $dungeons = $self->branches->{dungeons};
+    my $level = TAEB::World::Level->new(branch => $dungeons, z => 1);
+    $dungeons->levels([$level]);
+    $self->current_level($level);
+}
+
 1;
 
