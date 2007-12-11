740b59feb6a984f524e52574e9730e3bc6138846
Shawn M Moore <sartak@gmail.com>
2007-12-11 03:26:39 -0500

    Begin writing the Cartographer (sawtooth++)

---
+++ lib/TAEB/World/Cartographer.pm
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+package TAEB::World::Cartographer;
+use Moose;
+
+has dungeon => (
+    is       => 'rw',
+    isa      => 'TAEB::World::Dungeon',
+    weak_ref => 1,
+    required => 1,
+);
+
+sub update {
+    my $self  = shift;
+    my $level = $self->dungeon->current_level;
+
+    for my $y (1 .. 21) {
+        for my $x (0 .. 79) {
+            if ($taeb->vt->at($x, $y) ne $level->at($x, $y)->glyph) {
+                $level->update_tile($x, $y, $taeb->vt->at($x, $y));
+            }
+        }
+    }
+}
+
+1;
+
