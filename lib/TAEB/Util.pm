d53d24c7c865ea6bd2858bda9bd48b8d9f44f602
Shawn M Moore <sartak@gmail.com>
2007-12-11 03:25:32 -0500

    Start using Sub::Exporter in TAEB::Util

--- lib/TAEB/Util.pm
+++ lib/TAEB/Util.pm
@@ -2,11 +2,42 @@
 package TAEB::Util;
 use strict;
 use warnings;
+use Sub::Exporter -setup => {
+    exports => qw(tile_types glyph_to_type),
+};
+
+use List::Utils 'uniq';
+
+our %glyphs = (
+    "\0" => 'obscured',
+    ' '  => 'rock',
+    '|'  => 'wall',
+    '-'  => 'wall',
+    '.'  => 'floor',
+    ']'  => 'door',
+    ','  => 'door',
+    '>'  => 'stairs',
+    '<'  => 'stairs',
+    '^'  => 'trap',
+    '\\' => 'throne',
+
+    '_'  => [qw/grave altar/],
+    '{'  => [qw/sink fountain/],
+    '}'  => [qw/bars tree drawbridge water lava underwater/],
+
+    '#'  => 'corridor',
+    #'#'  => 'air', # who cares, no difference
+);
+
+our @glyphs = uniq map { ref $_ ? @$_ : $_ } values %glyphs;
 
 sub tile_types {
-    return qw(obscured rock wall floor door bars tree corridor stairs altar
-              grave throne sink fountain water ice lava drawbridge air cloud
-              underwater);
+    return @glyphs;
+}
+
+sub glyph_to_type {
+    my $glyph = shift;
+    return $glyphs{$glyph};
 }
 
 1;
