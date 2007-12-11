745655ecbbd2860cadf8f3267b8f068332b04deb
Shawn M Moore <sartak@gmail.com>
2007-12-11 03:26:17 -0500

    More work in TW::Tile

--- lib/TAEB/World/Tile.pm
+++ lib/TAEB/World/Tile.pm
@@ -25,5 +25,36 @@ has type => (
     default => 'obscured',
 );
 
+has glyph => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => "\0",
+);
+
+has floor_glyph => (
+    is      => 'rw',
+    isa     => 'Str',
+    default => "\0",
+);
+
+sub update {
+    my $self = shift;
+    my $newglyph = shift;
+
+    $self->glyph($newglyph);
+
+    # if glyph_to_type returns false, it's not a dungeon feature, it's an item
+    # or monster
+    my $type = glyph_to_type($newglyph) || 'obscured';
+    if (ref($type) eq 'ARRAY') {
+        # XXX: use ; to figure out which we're dealing with
+        $type = $type->[0];
+    }
+    $self->type($type);
+
+    $self->floor_glyph($newglyph)
+        unless $type eq 'obscured';
+}
+
 1;
 
