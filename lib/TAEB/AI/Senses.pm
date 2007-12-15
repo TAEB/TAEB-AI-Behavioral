ed6939adec06663a8208350f9506ae86cd8258ce
Shawn M Moore <sartak@gmail.com>
2007-12-15 16:19:45 -0500

    Add "in_wereform" to senses

--- lib/TAEB/AI/Senses.pm
+++ lib/TAEB/AI/Senses.pm
@@ -12,9 +12,16 @@ has maxhp => (
     isa => 'Int',
 );
 
+has in_wereform => (
+    is  => 'rw',
+    isa => 'Bool',
+);
+
 sub update {
     my $self = shift;
-    my $botl = $main::taeb->vt->row_plaintext(23);
+
+    my $status = $main::taeb->vt->row_plaintext(22);
+    my $botl   = $main::taeb->vt->row_plaintext(23);
 
     if ($botl =~ /HP:(\d+)\((\d+)\)/) {
         $self->hp($1);
@@ -23,6 +30,8 @@ sub update {
     else {
         $main::taeb->error("Unable to parse HP from '$botl'");
     }
+
+    $self->in_wereform($status =~ /^TAEB the Were/ ? 1 : 0);
 }
 
 1;
