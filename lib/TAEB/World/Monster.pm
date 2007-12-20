ef972da378b80707e565aff9886db29342f5d3fc
kelly530 <kelly530@umn.edu>
2007-12-20 01:57:55 -0500

    Flesh out Monster class.

--- lib/TAEB/World/Monster.pm
+++ lib/TAEB/World/Monster.pm
@@ -2,6 +2,52 @@
 package TAEB::World::Monster;
 use Moose;
 
+has 'id' => (
+  is  => 'rw',
+  isa => 'Str',
+);
+
+has 'name' => (
+  is  => 'rw',
+  isa => 'Str',
+);
+
+has 'ac' => (
+  is  => 'rw',
+  isa => 'Int',
+);
+
+has 'mr' => (
+  is  => 'rw',
+  isa => 'Int',
+);
+
+has 'lev' => (
+  is  => 'rw',
+  isa => 'Int',
+);
+
+has 'spd' => (
+  is  => 'rw',
+  isa => 'Int',
+);
+
+# whether the monster respects Elbereth
+has 'elbereth' => (
+  is      => 'rw',
+  isa     => 'Int',
+  default => 1,
+);
+
+has 'glyph' => (
+  is  => 'rw',
+  isa => 'Str',
+);
+
+has 'color' => (
+  is  => 'rw',
+  isa => 'Str',
+);
 
 1;
 
