392d8e0aceb8251bac4fea40ea60684fe07af3d0
Shawn M Moore <sartak@gmail.com>
2007-12-20 18:52:20 -0500

    Add a TAEB::Knowledge module which will load all of our knowledge

---
+++ lib/TAEB/Knowledge.pm
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+package TAEB::Knowledge;
+use strict;
+use warnings;
+
+use TAEB::Knowledge::Monster;
+
+use TAEB::Knowledge::Item::Food;
+use TAEB::Knowledge::Item::Weapon;
+
+1;
+
