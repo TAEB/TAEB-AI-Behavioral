4b891e28351e8594ddf358161bd07ff47371c77e
Shawn M Moore <sartak@gmail.com>
2007-12-20 21:09:12 -0500

    Whoops, we're zero-based. Right.

--- lib/TAEB/AI/Behavior/FixHunger.pm
+++ lib/TAEB/AI/Behavior/FixHunger.pm
@@ -6,7 +6,7 @@ extends 'TAEB::AI::Behavior';
 sub prepare {
     my $self = shift;
 
-    return TAEB->vt->row_plaintext(24) =~ /\bFai/ ? 1000 : 0;
+    return TAEB->vt->row_plaintext(23) =~ /\bFai/ ? 1000 : 0;
 }
 
 sub next_action { "#pray\n" }
