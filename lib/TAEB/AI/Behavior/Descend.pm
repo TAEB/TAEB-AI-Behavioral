b7dfa64ac41080e910b66ff5f83707b043f7c625
Shawn M Moore <sartak@gmail.com>
2007-12-20 20:56:33 -0500

    Add a Descend behavior

---
+++ lib/TAEB/AI/Behavior/Descend.pm
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+package TAEB::AI::Behavior::Descend;
+use Moose;
+extends 'TAEB::AI::Behavior';
+
+has path => (
+    is  => 'rw',
+    isa => 'TAEB::World::Path',
+);
+
+sub prepare {
+    my $self = shift;
+
+    # are we on >? if so, head down
+    if (TAEB->current_tile->floor_glyph eq '>') {
+        return 99;
+    }
+
+    # poor optimization: do we have a > on screen? (XXX: what if it's obscured?)
+    return 0 unless TAEB->map_like(qr/>/);
+
+    # find our >
+    my $path = TAEB::World::Path->first_match(
+        sub { shift->floor_glyph eq '>' },
+    );
+
+    $self->path($path);
+
+    return $path ? 50 : 0;
+}
+
+sub next_action {
+    my $self = shift;
+
+    # descend if able
+    return '>' if TAEB->current_tile->floor_glyph eq '>';
+
+    # otherwise, head towards >
+    substr($self->path->path, 0, 1);
+}
+
+sub currently { "Descending." }
+
+sub max_urgency { 99 }
+
+1;
+
