6817eb3d6c5b232da68d880765007234da47f080
Shawn M Moore <sartak@gmail.com>
2007-12-20 21:08:28 -0500

    FixHunger is not ready for feats of cleverness

--- lib/TAEB/AI/Behavior/FixHunger.pm
+++ lib/TAEB/AI/Behavior/FixHunger.pm
@@ -5,16 +5,13 @@ extends 'TAEB::AI::Behavior';
 
 sub prepare {
     my $self = shift;
-    my $urgency = 1000 - TAEB->hunger;
 
-    # start worrying about hunger a little before Hungry (150)
-    $urgency = 0 if $urgency < 800;
-    return $urgency;
+    return TAEB->vt->row_plaintext(24) =~ /\bFai/ ? 1000 : 0;
 }
 
 sub next_action { "#pray\n" }
 
-sub max_urgency { 1200 }
+sub max_urgency { 1000 }
 
 1;
 
