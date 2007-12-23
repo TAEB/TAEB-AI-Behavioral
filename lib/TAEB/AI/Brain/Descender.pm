2d3179334f6b9c1fb777fd8b618ca2a9c6c7921d
Shawn M Moore <sartak@gmail.com>
2007-12-23 12:52:30 -0500

    Add RandomWalk to Descender

--- lib/TAEB/AI/Brain/Descender.pm
+++ lib/TAEB/AI/Brain/Descender.pm
@@ -9,15 +9,16 @@ TAEB::AI::Brain::Descender - descend as quickly as sanely possible
 
 =cut
 
-sub autoload_behaviors { qw/Explore FixHunger Descend Search Doors/ }
+sub autoload_behaviors { qw/Explore FixHunger Descend Search Doors RandomWalk/ }
 
 sub weight_behaviors {
     return {
-        FixHunger => 10000,
-        Descend   => 5000,
-        Doors     => 4000,
-        Explore   => 2500,
-        Search    => 1000,
+        FixHunger  => 10000,
+        Descend    => 5000,
+        Doors      => 4000,
+        Explore    => 2500,
+        Search     => 1000,
+        RandomWalk => 100,
     };
 }
 
