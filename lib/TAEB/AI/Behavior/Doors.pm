9aa46cb4cf1297a274b67a6982f7975e7f58ef46
Shawn M Moore <sartak@gmail.com>
2007-12-23 11:53:53 -0500

    Add a Doors behavior

---
+++ lib/TAEB/AI/Behavior/Doors.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+package TAEB::AI::Behavior::Doors;
+use Moose;
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+
+    return 0 unless TAEB->senses->can_kick;
+
+    TAEB->each_adjacent(sub {
+        my ($tile, $dir) = @_;
+        if ($tile->glyph eq ']') {
+            $self->commands([chr(4) . $dir]);
+            $self->currently("Kicking down a door.");
+            return 100;
+        }
+    });
+
+    return 0 unless TAEB->map_like(qr/\]/);
+
+    my $path = TAEB::World::Path->first_match(sub { shift->glyph eq ']' });
+    $self->path($path);
+
+    return $path && length($path->path) ? 80 : 0;
+}
+
+1;
+
