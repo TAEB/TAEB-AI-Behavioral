ec1e2af3931b9b710637e04ddd0b6b8de9f19ae9
Shawn M Moore <sartak@gmail.com>
2007-12-23 13:41:07 -0500

    Reorganize Descender's weights so they're easy to follow

--- lib/TAEB/AI/Brain/Descender.pm
+++ lib/TAEB/AI/Brain/Descender.pm
@@ -11,13 +11,22 @@ TAEB::AI::Brain::Descender - descend as quickly as sanely possible
 
 sub weight_behaviors {
     return {
-        FixHunger  => 10000,
-        Fight      => 5000,
-        Descend    => 5000,
-        Doors      => 4000,
-        Explore    => 2500,
-        Search     => 1000,
-        RandomWalk => 100,
+        FixHunger          => 1_000_000,
+
+        Descend            => 80_000,
+        # Descend:            80_000
+        Fight              => 50_000,
+        # Elbereth:           50_000
+        # Attack:             45_000
+        # Path to downstairs: 40_000
+        # Path to monster:    25_000
+        Doors              => 10_000,
+        # Kick:               10_000
+        # Path to door:       5_000
+
+        Explore            => 2_500,
+        Search             => 1_000,
+        RandomWalk         => 1,
     };
 }
 
