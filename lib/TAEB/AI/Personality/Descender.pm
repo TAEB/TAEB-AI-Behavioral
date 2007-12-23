f380adb8d1ec42a4ca6cae6d47f873f197893863
Shawn M Moore <sartak@gmail.com>
2007-12-23 14:16:42 -0500

    Remove the now unnecessary comments from Descender's weight_behaviors. Yay!

--- lib/TAEB/AI/Personality/Descender.pm
+++ lib/TAEB/AI/Personality/Descender.pm
@@ -12,18 +12,9 @@ TAEB::AI::Personality::Descender - descend as quickly as sanely possible
 sub weight_behaviors {
     return {
         FixHunger          => 1_000_000,
-
         Descend            => 80_000,
-        # Descend:            80_000
         Fight              => 50_000,
-        # Elbereth:           50_000
-        # Attack:             45_000
-        # Path to downstairs: 40_000
-        # Path to monster:    25_000
         Doors              => 10_000,
-        # Kick:               10_000
-        # Path to door:       5_000
-
         Explore            => 2_500,
         Search             => 1_000,
         RandomWalk         => 1,
