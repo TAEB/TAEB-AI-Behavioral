7c369d8041dadb66590c86c7ff628e1a420a061b
Shawn M Moore <sartak@gmail.com>
2008-01-01 14:25:43 -0500

    Some comments in Fight

--- lib/TAEB/AI/Behavior/Fight.pm
+++ lib/TAEB/AI/Behavior/Fight.pm
@@ -6,12 +6,14 @@ extends 'TAEB::AI::Behavior';
 sub prepare {
     my $self = shift;
 
+    # if we're at 50% health or less and we can write Elbereth, do it
     if (TAEB->hp * 2 < TAEB->maxhp && !TAEB->senses->in_wereform) {
         $self->currently("Writing Elbereth.");
         $self->commands(["E-  Elbereth\n"]);
         return 100;
     }
 
+    # if there's an adjacent monster, attack it
     my $found_monster;
     TAEB->each_adjacent(sub {
         my ($tile, $dir) = @_;
@@ -23,12 +25,15 @@ sub prepare {
     });
     return 90 if $found_monster;
 
+    # shortcut: if nothing on the map looks like a monster, don't do pathfinding
     return 0 unless TAEB->map_like(qr/[a-zA-Z~&';:]/);
 
+    # look for the nearest tile with a monster
     my $path = TAEB::World::Path->first_match(
         sub { shift->has_monster },
     );
 
+    # there's a monster on the map, but we don't know how to reach it
     return 0 unless $path && $path->path;
 
     $self->currently("Heading towards a " . $path->to->glyph . " monster");
