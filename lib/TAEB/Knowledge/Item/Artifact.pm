343fe1f5f615b981fc220d7a53ec04aef49cb1af
Shawn M Moore <sartak@gmail.com>
2008-01-04 03:14:50 -0500

    Add Knowledge::Item::Artifact, which is composed of artifacts from all the other items

---
+++ lib/TAEB/Knowledge/Item/Artifact.pm
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+package TAEB::Knowledge::Item::Artifact;
+use MooseX::Singleton;
+use TAEB::Knowledge::Weapon;
+
+has list => (
+    is      => 'ro',
+    isa     => 'HashRef',
+    lazy    => 1,
+    default => sub {
+        my $artifacts = { };
+
+        for (qw/Weapon/) {
+            my $list = "TAEB::Knowledge::$_"->list;
+            while (my ($name, $stats) = each %$list) {
+                next unless $stats->{artifact};
+                $artifacts->{$name} = $stats;
+            }
+        }
+
+        return $foods;
+    },
+);
+
+sub artifact {
+    my $self = shift;
+    my $item = TAEB::Knowledge::Item->canonicalize_item(shift);
+
+    return $self->list->{$item};
+}
+
+sub seen {
+    my $self = shift;
+    my $name = shift;
+    my $artifact = $self->artifact($name) or do {
+        warn "No artifact found for '$name'.";
+        return 0;
+    };
+
+    if (@_) {
+        return $artifact->{seen} = shift;
+    }
+    return $artifact->{seen};
+}
+
+1;
+
