5c56995f0408042d23dcae3a342307f05d548188
Shawn M Moore <sartak@gmail.com>
2008-01-04 02:02:25 -0500

    Start fleshing out Item

--- lib/TAEB/World/Item.pm
+++ lib/TAEB/World/Item.pm
@@ -2,6 +2,26 @@
 package TAEB::World::Item;
 use Moose;
 
+has appearance => (
+    is       => 'rw',
+    isa      => 'Str',
+    required => 1,
+);
+
+=head2 matches Str -> Bool
+
+Does the given item look sufficiently like this item?
+
+This is intentionally vague because I don't know what I want yet.
+
+=cut
+
+sub matches {
+    my $self = shift;
+    my $item = shift;
+
+    $self->appearance eq $item;
+}
 
 1;
 
