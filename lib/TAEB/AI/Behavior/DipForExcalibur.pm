bee3328989242c4b3ed223d658ec3ab32a66bc5a
Shawn M Moore <sartak@gmail.com>
2008-01-04 02:03:25 -0500

    Add a DipForExcalibur method, which doesn't have all the necessary framework parts ready, but damn is it cool

---
+++ lib/TAEB/AI/Behavior/DipForExcalibur.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+package TAEB::AI::Behavior::DipForExcalibur;
+use Moose;
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+
+    # are we eligible to dip for Excalibur?
+    return 0 unless TAEB->level >= 5;
+    return 0 unless TAEB->align eq 'Law';
+
+    # only one Excalibur. Alas.
+    return 0 if TAEB::Knowledge::Item->seen_artifact("Excalibur");
+
+    # do we have a long sword to dip in our inventory?
+    my $longsword = TAEB->inventory->find_item("long sword")
+        or return 0;
+
+    # are we standing on a fountain? if so, dip!
+    if (TAEB->current_tile->type eq 'fountain') {
+        $self->currently("Dipping for Excalibur!");
+        $self->next("#dip\n" . $longsword->glyph . "y");
+        return 100;
+    }
+
+    # find a fountain
+    my $path = TAEB::World::Path->first_match(
+        sub { shift->type eq 'fountain' },
+    );
+
+    $self->currently("Heading towards a fountain");
+    $self->path($path);
+    return $path ? 50 : 0;
+}
+
+sub weights {
+    return {
+        100 => "dipping for Excalibur",
+         50 => "path to fountain",
+    };
+}
+
+1;
+
