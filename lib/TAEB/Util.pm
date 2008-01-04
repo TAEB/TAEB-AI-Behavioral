d7641515e7d6a78cb3eca3bae6514cc3ea397879
Shawn M Moore <sartak@gmail.com>
2008-01-03 23:05:39 -0500

    Refactor TAEB::Util a little bit so we export our color constants

--- lib/TAEB/Util.pm
+++ lib/TAEB/Util.pm
@@ -2,31 +2,36 @@
 package TAEB::Util;
 use strict;
 use warnings;
+
+BEGIN {
+    our %colors = (
+        COLOR_BLACK          => 0,
+        COLOR_RED            => 1,
+        COLOR_GREEN          => 2,
+        COLOR_BROWN          => 3,
+        COLOR_BLUE           => 4,
+        COLOR_MAGENTA        => 5,
+        COLOR_CYAN           => 6,
+        COLOR_GRAY           => 7,
+        COLOR_NONE           => 8,
+        COLOR_ORANGE         => 9,
+        COLOR_BRIGHT_GREEN   => 10,
+        COLOR_YELLOW         => 11,
+        COLOR_BRIGHT_BLUE    => 12,
+        COLOR_BRIGHT_MAGENTA => 13,
+        COLOR_BRIGHT_CYAN    => 14,
+        COLOR_WHITE          => 15,
+        COLOR_MAX            => 16,
+    );
+}
+
+use constant \%colors;
+
 use Sub::Exporter -setup => {
-    exports => [qw(tile_types glyph_to_type direction)],
+    exports => [qw(tile_types glyph_to_type direction), keys %colors],
 };
 
 use List::MoreUtils 'uniq';
-
-use constant {
-    COLOR_BLACK          => 0,
-    COLOR_RED            => 1,
-    COLOR_GREEN          => 2,
-    COLOR_BROWN          => 3,
-    COLOR_BLUE           => 4,
-    COLOR_MAGENTA        => 5,
-    COLOR_CYAN           => 6,
-    COLOR_GRAY           => 7,
-    COLOR_NONE           => 8,
-    COLOR_ORANGE         => 9,
-    COLOR_BRIGHT_GREEN   => 10,
-    COLOR_YELLOW         => 11,
-    COLOR_BRIGHT_BLUE    => 12,
-    COLOR_BRIGHT_MAGENTA => 13,
-    COLOR_BRIGHT_CYAN    => 14,
-    COLOR_WHITE          => 15,
-    COLOR_MAX            => 16,
-};
 
 our %glyphs = (
     ' '  => 'rock',
