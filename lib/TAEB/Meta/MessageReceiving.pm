450aa108b52163518126521bca9e840063184672
Shawn M Moore <sartak@gmail.com>
2008-01-06 01:43:08 -0500

    Begin writing the message subsystem, using a Moose role for pubsub :)

---
+++ lib/TAEB/Meta/MessageReceiving.pm
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+package TAEB::Meta::MessageReceiving;
+use Moose::Role;
+
+=head1 NAME
+
+TAEB::Meta::MessageReceiving - role for subscribing to NetHack messages
+
+=cut
+
+=head1 DESCRIPTION
+
+=head1 CALLBACKS
+
+=head2 msg_status_change Str[, Bool]
+
+TAEB's status has changed. The first argument is a string representation
+of the status that changed, and the second argument, when applicable, indicates
+whether TAEB gained or lost the status. For example, when the message "You are
+blinded by a blast of light!" is received, this method will be invoked with
+the first argument "blindness" and the second argument true. When the message
+"You can see again." is received, this method will be invoked with the
+first argument "blindness" and the second argument false.
+
+=cut
+
+sub msg_status_change { }
+
+1;
+
