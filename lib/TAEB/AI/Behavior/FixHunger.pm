dc9c437a9857b4850dd3b61d53db3195e04177fd
bagofholding <bagofholding@gmail.com>
2008-01-07 04:28:29 -0500

    Eat food from inventory when hungry

--- lib/TAEB/AI/Behavior/FixHunger.pm
+++ lib/TAEB/AI/Behavior/FixHunger.pm
@@ -13,9 +13,13 @@ sub prepare {
     }
 
     if (TAEB->senses->nutrition < 400) {
-        $self->next("e...");
-        $self->currently("Eating food.");
-        return 0;
+        for my $item (TAEB->inventory->items) {
+            if (TAEB::Knowledge::Item::Food->should_eat($item->appearance)) {
+                $self->next("e" . $item->slot);
+                $self->currently("Eating food.");
+                return 100;
+            }
+        }
     }
 
     return 0;
