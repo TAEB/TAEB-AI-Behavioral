f46d14e567ab246741a635cfaf4f676b816e7880
Shawn M Moore <sartak@gmail.com>
2008-01-06 21:35:17 -0500

    Refactor the radiate code out of Projectiles and into Level

--- lib/TAEB/AI/Behavior/Projectiles.pm
+++ lib/TAEB/AI/Behavior/Projectiles.pm
@@ -3,8 +3,6 @@ package TAEB::AI::Behavior::Projectiles;
 use Moose;
 extends 'TAEB::AI::Behavior';
 
-use TAEB::Util 'direction', 'deltas';
-
 sub prepare {
     my $self = shift;
     my $direction;
@@ -13,30 +11,13 @@ sub prepare {
     my $projectile = TAEB->inventory->find_item($self->can('pickup'))
         or return;
 
-    # XXX: this needs to be refactored into the framework
-    # check each direction...
-    DIRECTION: for (deltas) {
-        my ($dx, $dy) = @$_;
-        my ($x, $y) = (TAEB->x, TAEB->y);
+    my $direction = TAEB->current_level->radiate(
+        sub { shift->has_monster },
 
-        # check up to six tiles out
-        # XXX: TAEB could calculate how far he could throw $projectile..
-        for (1 .. 6) {
-            $x += $dx; $y += $dy;
-            my $tile = TAEB->current_level->at($x, $y) or next DIRECTION;
-
-            if ($tile->has_monster) {
-                $direction = direction($x, $y);
-                last DIRECTION;
-            }
-
-            # this check could be better. we don't want to throw a dagger if:
-            #   |...@.|#j
-
-            $tile->is_walkable or next DIRECTION;
-
-        }
-    }
+        # how far to radiate. we can eventually calculate how far $projectile
+        # can travel..!
+        max => 6,
+    );
 
     # no monster found
     return 0 if !$direction;
--- lib/TAEB/World/Level.pm
+++ lib/TAEB/World/Level.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package TAEB::World::Level;
 use Moose;
+use TAEB::Util qw/deltas direction/;
 
 has tiles => (
     is      => 'rw',
@@ -56,6 +57,66 @@ sub step_on {
     my $y = shift;
 
     $self->tiles->[$y][$x]->step_on;
+}
+
+=head2 radiate CODE[, ARGS] -> (Str | (Str, Int))
+
+This method will radiate in the eight NetHack directions. It will call the
+coderef for each tile encountered. The coderef will receive the tile as its
+argument. Once the coderef returns a true value, then the radiating stops and
+something will be returned:
+
+If called in scalar context, the vi-key direction will be returned. If called
+in list context, the vi-key direction and distance will be returned.
+
+The optional arguments are:
+
+=over 4
+
+=item max (default: 80)
+
+How far to radiate outwards. You probably can't throw a dagger all the way
+across the level, so you may want to decrease it to something more realistic.
+Like 3, har har. You're weak.
+
+=back
+
+=cut
+
+sub radiate {
+    my $self = shift;
+    my $code = shift;
+    my %args = (
+        max => 80, # hey, we may want to throw all the way across the level..
+        @_,
+    );
+
+    # check each direction
+    DIRECTION: for (deltas) {
+        my ($dx, $dy) = @$_;
+        my ($x, $y) = (TAEB->x, TAEB->y);
+
+        for (1 .. $args{max}) {
+            $x += $dx; $y += $dy;
+
+            # have we fallen off the map? if so, stop this line of radiation
+            my $tile = TAEB->current_level->at($x, $y) or next DIRECTION;
+
+            my $ret = $code->($tile);
+            if ($ret) {
+                # if they ask for a scalar, give them the direction
+                return direction($x, $y) if !wantarray;
+
+                # if they ask for a list, give them (direction, distance)
+                return (direction($x, $y), $_);
+            }
+
+            # stop radiating
+            $tile->is_walkable or next DIRECTION;
+
+        }
+    }
+
 }
 
 1;
