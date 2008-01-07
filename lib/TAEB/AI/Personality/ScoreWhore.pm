93e15cc08837af3569bdc2d30834732f7c5e20eb
Shawn M Moore <sartak@gmail.com>
2008-01-07 01:40:58 -0500

    Add a ScoreWhore personality

---
+++ lib/TAEB/AI/Personality/ScoreWhore.pm
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+package TAEB::AI::Personality::ScoreWhore;
+use Moose;
+extends 'TAEB::AI::Personality::Behavioral';
+
+has time_spent_on => (
+    is            => 'rw',
+    isa           => 'HashRef[Int]',
+    default       => sub { {} },
+    documentation => "How much time was spent (in other words, how many actions were made) on each level",
+);
+
+=head1 NAME
+
+TAEB::AI::Personality::ScoreWhore - milk each dungeon level for as long as possible
+
+=head1 DESCRIPTION
+
+This personality will avoid descending until its experience level is greater
+than its dungeon level, or it spent 1000 turns on the current level.
+
+This is an example of shifting the weight of a behavior around.
+
+=cut
+
+sub weight_behaviors {
+    my $self = shift;
+
+    my $behaviors = {
+        FixHunger          => 1_000_000,
+        Heal               => 750_000,
+        Fight              => 50_000,
+        Projectiles        => 49_000,
+        DipForExcalibur    => 24_500,
+        GetItems           => 24_000,
+        Doors              => 10_000,
+        Explore            => 2_500,
+        Search             => 1_000,
+        Descend            => 2,
+        RandomWalk         => 1,
+    };
+
+    # Descend at a very leisurely pace
+    $behaviors->{Descend} = 2000 if TAEB->level > TAEB->z
+                                 || $self->time_spent_on->{TAEB->z}++ >= 1000;
+
+    return $behaviors;
+}
+
+1;
+
