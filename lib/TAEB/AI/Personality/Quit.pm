5b35649292ded3f55dc87a6bdffc1701f1f8b864
Shawn M Moore <sartak@gmail.com>
2008-01-09 23:26:50 -0500

    Add a Quit personality

---
+++ lib/TAEB/AI/Personality/Quit.pm
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+package TAEB::AI::Personality::Quit;
+use Moose;
+extends 'TAEB::AI::Personality';
+
+=head1 NAME
+
+TAEB::AI::Personality::Quit - I just can't take it any more...
+
+=cut
+
+sub next_action { "#quit\ny" }
+
+1;
+
