b088b4ff94b6465ad181b3ee24e4af5db165f753
Shawn M Moore <sartak@gmail.com>
2008-01-09 19:52:03 -0500

    Special case personality in TAEB->send_messages

--- lib/TAEB.pm
+++ lib/TAEB.pm
@@ -431,11 +431,13 @@ sub send_messages {
     my @msgs = splice @{ $self->deferred_messages };
 
     for (@msgs) {
+        TAEB->personality->send_message(@$_);
+
         my $msgname = shift @$_;
         TAEB->debug("Dequeueing message $msgname.");
 
         # this list should not be hardcoded. ideas?
-        for my $recipient (TAEB->personality, TAEB->senses, TAEB->dungeon->cartographer, "TAEB::Knowledge::Item::Artifact") {
+        for my $recipient (TAEB->senses, TAEB->dungeon->cartographer, "TAEB::Knowledge::Item::Artifact") {
             $recipient->$msgname(@$_)
                 if $recipient->can($msgname);
         }
