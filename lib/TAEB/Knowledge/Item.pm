7b4786fc80ba828879b96641dd7dc215256a9ef6
jluehrs2 <jluehrs2@uiuc.edu>
2008-01-09 21:01:32 -0500

    add a hash table to TAEB::Knowledge::Item to determine the item class of a given item

--- lib/TAEB/Knowledge/Item.pm
+++ lib/TAEB/Knowledge/Item.pm
@@ -9,6 +9,25 @@ use TAEB::Knowledge::Item::Tool;
 use TAEB::Knowledge::Item::Artifact;
 use TAEB::Knowledge::Item::Armor;
 
+has list => (
+    is      => 'ro',
+    isa     => 'HashRef',
+    lazy    => 1,
+    default => sub {
+        my $items = {};
+
+        for (qw/Weapon Armor Tool Food/) {
+            my $list = "TAEB::Knowledge::Item::$_"->list;
+            while (my ($name, $stats) = each %$list) {
+                $items->{$name} = lc $_;
+                $items->{$stats->{appearance}} = lc $_;
+            }
+        }
+
+        return $items;
+    },
+);
+
 sub canonicalize_item {
     my $self = shift;
     my $item = shift;
