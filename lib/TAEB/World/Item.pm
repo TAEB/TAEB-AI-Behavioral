2b66585ea44849b97055b5f2fcbb2b4c9ce92373
jluehrs2 <jluehrs2@uiuc.edu>
2008-01-10 22:09:09 -0500

    don't let $class be undef

--- lib/TAEB/World/Item.pm
+++ lib/TAEB/World/Item.pm
@@ -199,7 +199,7 @@ sub trigger_appearance {
     $self->is_fooproof(1)           if defined $proof;
     $self->enchantment($spe)        if defined $spe;
     if (defined $item) {
-        my $class = TAEB::Knowledge::Item->list->{$item};
+        my $class = TAEB::Knowledge::Item->list->{$item} || '';
         $self->class('gold')   if $item =~ /gold piece/;
         $self->class('weapon') if $class eq 'weapon';
         $self->class('armor')  if $class eq 'armor';
