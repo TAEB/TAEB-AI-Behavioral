b4470f7e2c4d374fc5912a3be7f24e1590ed155f
Shawn M Moore <sartak@gmail.com>
2008-01-10 22:45:25 -0500

    We don't want TAEB escaping just yet

--- lib/TAEB/AI/Behavior/Ascend.pm
+++ lib/TAEB/AI/Behavior/Ascend.pm
@@ -6,14 +6,13 @@ extends 'TAEB::AI::Behavior';
 sub prepare {
     my $self = shift;
 
+    # we don't actually want TAEB escaping just yet
+    return 0 if TAEB->z == 1;
+
     # are we on <? if so, head up
     if (TAEB->current_tile->floor_glyph eq '<') {
         $self->currently("Ascending");
         $self->next('<');
-        #special case to really leave the dungeon
-        if (TAEB->z == 1) {
-            $self->next('<y     ');
-        }
         return 100;
     }
 
