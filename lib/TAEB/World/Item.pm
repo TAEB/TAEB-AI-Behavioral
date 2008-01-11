26e82a27b1caad133cb816ae55d59d3a47040cc4
jluehrs2 <jluehrs2@uiuc.edu>
2008-01-11 17:39:49 -0500

    handle candles attached to the candelabrum

--- lib/TAEB/World/Item.pm
+++ lib/TAEB/World/Item.pm
@@ -111,6 +111,12 @@ has is_lit => (
     default => 0,
 );
 
+has candles_attached => (
+    is      => 'rw',
+    isa     => 'Int',
+    default => 0,
+);
+
 has is_equipped => (
     is      => 'rw',
     isa     => 'Bool',
@@ -161,47 +167,50 @@ sub trigger_appearance {
     # "foo named bar" and an item called "foo" and named "bar". similarly for
     # an item called "foo (0:1)". so... don't do that!
     my ($slot, $num, $buc, $greased, $poisoned, $ero1, $ero2, $used, $proof,
-        $spe, $item, $call, $name, $charge, $max_charge, $lit, $is_equipped) =
-        $appearance =~
-        m{(?:(\w)\s[+-])?\s*                             # inventory slot
-          (an?|the|\d+)\s*                               # number
-          (blessed|(?:un)?cursed)?\s*                    # cursedness
-          (greased)?\s*                                  # greasy
-          (poisoned)?\s*                                 # poisoned
-          ((?:(?:very|thoroughly) )?burnt|rusty)?\s*     # erosion 1
-          ((?:(?:very|thoroughly) )?rotted|corroded)?\s* # erosion 2
-          (partly used)?\s*                              # usedness
-          (fixed|(?:fire|rust|corrode)proof)?\s*         # fooproof
-          ([+-]\d+)?\s*                                  # enchantment
-          (.*?)\s*                                       # item name
-          (called .*?)?\s*                               # non-specific name
-          (named .*?)?\s*                                # specific name
-          (?:\((\d+):(\d+)\))?\s*                        # charges
-          (\(lit\))?\s*                                  # lit
-          (\(.*\))?\s*                                   # equipped
-          $                                              # anchor the regex
+        $spe, $item, $call, $name, $charge, $max_charge, $ncandles,
+        $lit_candelabrum, $lit, $is_equipped) = $appearance =~
+        m{(?:(\w)\s[+-])?\s*                               # inventory slot
+          (an?|the|\d+)\s*                                 # number
+          (blessed|(?:un)?cursed)?\s*                      # cursedness
+          (greased)?\s*                                    # greasy
+          (poisoned)?\s*                                   # poisoned
+          ((?:(?:very|thoroughly) )?burnt|rusty)?\s*       # erosion 1
+          ((?:(?:very|thoroughly) )?rotted|corroded)?\s*   # erosion 2
+          (partly used)?\s*                                # usedness
+          (fixed|(?:fire|rust|corrode)proof)?\s*           # fooproof
+          ([+-]\d+)?\s*                                    # enchantment
+          (.*?)\s*                                         # item name
+          (called .*?)?\s*                                 # non-specific name
+          (named .*?)?\s*                                  # specific name
+          (?:\((\d+):(\d+)\))?\s*                          # charges
+          (?:\((no|[1-7]) candles?(, lit| attached)\))?\s* # lit candelabrum
+          (\(lit\))?\s*                                    # lit
+          (\(.*\))?\s*                                     # equipped
+          $                                                # anchor the regex
          }x;
 
-    $num = 1 if $num =~ /\w/;
+    $num = 1        if $num =~ /\w/;
     $spe =~ s/^\+// if defined $spe;
+    $ncandles = 0   if (defined $ncandles && $ncandles =~ /no/);
+    $lit = 1        if (defined $lit_candelabrum && $lit_candelabrum =~ /lit/);
     # XXX: depluralization and japanese name mappings should go here
 
-    $self->slot($slot)              if defined $slot;
-    $self->quantity($num)           if defined $num;
-    $self->buc($buc)                if defined $buc;
-    $self->is_greased(1)            if defined $greased;
+    $self->slot($slot)                 if defined $slot;
+    $self->quantity($num)              if defined $num;
+    $self->buc($buc)                   if defined $buc;
+    $self->is_greased(1)               if defined $greased;
     if (defined $ero1) {
         $self->erosion1(1);
-        $self->erosion1(2)          if $ero1 =~ /very/;
-        $self->erosion1(3)          if $ero1 =~ /thoroughly/;
+        $self->erosion1(2)             if $ero1 =~ /very/;
+        $self->erosion1(3)             if $ero1 =~ /thoroughly/;
     }
     if (defined $ero2) {
         $self->erosion2(1);
-        $self->erosion2(2)          if $ero2 =~ /very/;
-        $self->erosion2(3)          if $ero2 =~ /thoroughly/;
+        $self->erosion2(2)             if $ero2 =~ /very/;
+        $self->erosion2(3)             if $ero2 =~ /thoroughly/;
     }
-    $self->is_fooproof(1)           if defined $proof;
-    $self->enchantment($spe)        if defined $spe;
+    $self->is_fooproof(1)              if defined $proof;
+    $self->enchantment($spe)           if defined $spe;
     if (defined $item) {
         my $class = TAEB::Knowledge::Item->list->{$item};
 
@@ -210,23 +219,25 @@ sub trigger_appearance {
             $class = '';
         }
 
-        $self->class('gold')   if $item =~ /gold piece/;
-        $self->class('weapon') if $class eq 'weapon';
-        $self->class('armor')  if $class eq 'armor';
-        $self->class('food')   if $class eq 'food';
-        $self->class('scroll') if $item =~ /scroll/;
-        $self->class('book')   if $item =~ /[bB]ook/;
-        $self->class('potion') if $item =~ /potion/;
-        $self->class('amulet') if $item =~ /[aA]mulet/;
+        $self->class('gold')           if $item =~ /gold piece/;
+        $self->class('weapon')         if $class eq 'weapon';
+        $self->class('armor')          if $class eq 'armor';
+        $self->class('food')           if $class eq 'food';
+        $self->class('scroll')         if $item =~ /scroll/;
+        $self->class('book')           if $item =~ /[bB]ook/;
+        $self->class('potion')         if $item =~ /potion/;
+        $self->class('amulet')         if $item =~ /[aA]mulet/;
         # this won't catch identified rings, but we need to not have this match
         # 'ring mail'. probably fix this once we have an item db for rings.
-        $self->class('ring')   if $item =~ /ring$/;
-        $self->class('wand')   if $item =~ /wand/;
-        $self->class('tool')   if $class eq 'tool';
-        $self->class('gem')    if 0; # don't match 'rock mole corpse', etc
+        $self->class('ring')           if $item =~ /ring$/;
+        $self->class('wand')           if $item =~ /wand/;
+        $self->class('tool')           if $class eq 'tool';
+        # don't match 'rock mole corpse', etc
+        $self->class('gem')            if 0;
     }
-    $self->charges($charge)         if defined $charge;
-    $self->max_charges($max_charge) if defined $max_charge;
+    $self->charges($charge)            if defined $charge;
+    $self->max_charges($max_charge)    if defined $max_charge;
+    $self->candles_attached($ncandles) if defined $ncandles;
     if ($self->class) {
         if ($self->class =~ /weapon|armor|food|tool/) {
             my $class = $self->class;
@@ -247,10 +258,10 @@ sub trigger_appearance {
                 if defined $spe || defined $charge;
         }
     }
-    $self->generic_name($call)      if defined $call;
-    $self->specific_name($name)     if defined $name;
-    $self->is_lit(1)                if defined $lit;
-    $self->is_equipped(1)           if defined $is_equipped;
+    $self->generic_name($call)         if defined $call;
+    $self->specific_name($name)        if defined $name;
+    $self->is_lit(1)                   if defined $lit;
+    $self->is_equipped(1)              if defined $is_equipped;
 }
 
 1;
