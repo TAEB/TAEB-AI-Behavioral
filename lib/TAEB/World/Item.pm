4e84b626e9e26cef903bbea16fb8dce52f0d6b62
jluehrs2 <jluehrs2@uiuc.edu>
2008-01-13 00:38:14 -0500

    remove old code that will need to be rewritten, and rename trigger_appearance to new_item, since that's what its purpose will now be

--- lib/TAEB/World/Item.pm
+++ lib/TAEB/World/Item.pm
@@ -153,43 +153,7 @@ has cost => (
     default => 0,
 );
 
-# check whether this is an artifact, and if so, let the artifact-tracker know
-# we've seen it
-sub BUILD {
-    my $self = shift;
-
-    my $artifact = TAEB::Knowledge::Item::Artifact->artifact($self->appearance)
-        or return;
-    TAEB::Knowledge::Item::Artifact->seen($self->appearance => 1);
-}
-
-=head2 matches (Str|Regexp|CODE) -> Bool
-
-Does the given item look sufficiently like this item?
-
-This is intentionally vague because I don't know what I want yet.
-
-If a coderef is passed in, then C<$_> will be the appearance. The coderef will
-also get an argument: the item itself.
-
-=cut
-
-sub matches {
-    my $self = shift;
-    my $item = shift;
-
-    if (ref($item) eq 'Regexp') {
-        return $self->appearance =~ $item;
-    }
-    elsif (ref($item) eq 'CODE') {
-        local $_ = $self->appearance;
-        return $item->($self);
-    }
-
-    return $self->appearance eq $item;
-}
-
-sub trigger_appearance {
+sub new_item {
     my $self       = shift;
     my $appearance = shift;
 
