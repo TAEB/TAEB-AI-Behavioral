e164d3dedd6b49c5b0ffa3c78a200dd0058df5ca
Shawn M Moore <sartak@gmail.com>
2008-01-14 00:05:46 -0500

    Add an engrave_useful method from EliteBot, which tells you if you'll learn anything from engrave-IDing

--- lib/TAEB/Knowledge/Item/Wand.pm
+++ lib/TAEB/Knowledge/Item/Wand.pm
@@ -14,5 +14,50 @@ sub all_identities {
     return keys %{ TAEB::Spoilers::Item::Wand->list };
 }
 
+my @_groups = (
+    [ 'cold'          ],
+    [ 'polymorph'     ],
+    [ 'speed monster' ],
+    [ 'slow monster'  ],
+    [ 'striking'      ],
+    [ 'magic missile' ],
+    [ 'light'         ],
+    [ 'enlightenment' ],
+    [ 'create monster'],
+    [ 'digging'       ],
+    [ 'fire'          ],
+    [ 'lightning'     ],
+    [ 'wishing'       ],
+
+    [ qw/sleep death/ ],
+    [ 'teleportation', 'make invisible', 'cancellation' ],
+
+    # these no-message wands are excluded because we cannot distinguish
+    # between engrave-giving-no-message and out-of-charges. bones piles, eg
+    #[ 'locking', 'nothing', 'opening', 'probing', 'undead turning', 'secret door detection' ],
+);
+
+my @groups = map { { map { +"wand of $_" => 1 } @$_ } } @_groups;
+
+sub engrave_useful {
+    my $self = shift;
+
+    # How this works is we group each wand identity based on what it would
+    # do in an engrave ID. For example, sleep and death would go into one
+    # group, and invis, tele, canc would go into another. If we have
+    # possible identities from two or more groups, then engrave-IDing would
+    # be useful in the sense that it'd rule some possibilities out.
+
+    my $appearance_groups = 0;
+    for my $group (@groups) {
+        for my $possibility ($self->possibilities) {
+            next unless $group->{$possibility};
+            return 1 if $appearance_groups++ > 0;
+        }
+    }
+
+    return 0;
+}
+
 1;
 
