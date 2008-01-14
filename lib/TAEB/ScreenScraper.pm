bdc4f0711be05e6e431ec229db6665db66fe2765
Shawn M Moore <sartak@gmail.com>
2008-01-13 21:06:59 -0500

    Split messages across --More--

--- lib/TAEB/ScreenScraper.pm
+++ lib/TAEB/ScreenScraper.pm
@@ -105,7 +105,7 @@ sub handle_more {
     # while there's a --More-- on the screen..
     while (TAEB->vt->contains("--More--")) {
         # add the text to the buffer
-        $self->messages($self->messages . TAEB->topline);
+        $self->messages($self->messages . '  ' . TAEB->topline);
 
         # try to get rid of the --More--
         TAEB->write(' ');
