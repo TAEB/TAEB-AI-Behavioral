824973418145dfb3d411f904554d8194f584c4f9
Shawn M Moore <sartak@gmail.com>
2008-03-07 05:10:34 -0500

    Add an Explorer personality which will take stairs after the Explore personality is exhausted

---
+++ lib/TAEB/AI/Personality/Explorer.pm
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+package TAEB::AI::Personality::Explorer;
+use Moose;
+extends 'TAEB::AI::Personality::Behavioral';
+
+=head1 NAME
+
+TAEB::AI::Personality::Explore - descend only after exploring the level
+
+=cut
+
+sub weight_behaviors {
+    return {
+        FixHunger          => 1_000_000,
+        Heal               => 750_000,
+        Fight              => 50_000,
+        Projectiles        => 49_000,
+        DipForExcalibur    => 24_500,
+        GetItems           => 24_000,
+        Doors              => 10_000,
+        Explore            => 2_500,
+        Descend            => 1_000,
+        Search             => 300,
+        RandomWalk         => 1,
+    };
+}
+
+make_immutable;
+
+1;
+
