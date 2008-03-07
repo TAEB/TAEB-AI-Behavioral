9c75b583a6999c2297da5e5fce1f2e3829190007
Shawn M Moore <sartak@gmail.com>
2008-03-07 05:40:58 -0500

    Make Descend and Ascend use GotoTile

--- lib/TAEB/AI/Behavior/Ascend.pm
+++ lib/TAEB/AI/Behavior/Ascend.pm
@@ -1,37 +1,17 @@
 #!/usr/bin/env perl
 package TAEB::AI::Behavior::Ascend;
 use Moose;
-extends 'TAEB::AI::Behavior';
+extends 'TAEB::AI::Behavior::GotoTile';
 
-sub prepare {
-    my $self = shift;
-
-    # we don't actually want TAEB escaping just yet
-    return 0 if TAEB->z == 1;
-
-    # are we on <? if so, head up
-    if (TAEB->current_tile->floor_glyph eq '<') {
-        $self->currently("Ascending");
-        $self->next('<');
-        return 100;
-    }
-
-    # find our <
-    my $path = TAEB::World::Path->first_match(
-        sub { shift->floor_glyph eq '<' },
-    );
-
-    $self->if_path($path => "Heading towards the upstairs");
+sub match_tile {
+    shift->floor_glyph eq '<' ? ('<', 'Ascending') : undef
 }
 
-sub urgencies {
-    return {
-        100 => "ascending stairs",
-         50 => "path to upstairs",
-    };
-}
+use constant tile_description => 'the upstairs';
+use constant using_urgency    => 'ascending';
 
 make_immutable;
 
 1;
+
 
--- lib/TAEB/AI/Behavior/Descend.pm
+++ lib/TAEB/AI/Behavior/Descend.pm
@@ -1,32 +1,14 @@
 #!/usr/bin/env perl
 package TAEB::AI::Behavior::Descend;
 use Moose;
-extends 'TAEB::AI::Behavior';
+extends 'TAEB::AI::Behavior::GotoTile';
 
-sub prepare {
-    my $self = shift;
-
-    # are we on >? if so, head down
-    if (TAEB->current_tile->floor_glyph eq '>') {
-        $self->currently("Descending");
-        $self->next('>');
-        return 100;
-    }
-
-    # find our >
-    my $path = TAEB::World::Path->first_match(
-        sub { shift->floor_glyph eq '>' },
-    );
-
-    $self->if_path($path => "Heading towards the downstairs");
+sub match_tile {
+    shift->floor_glyph eq '>' ? ('>', 'Descending') : undef
 }
 
-sub urgencies {
-    return {
-        100 => "descending",
-         50 => "path to downstairs",
-    };
-}
+use constant tile_description => 'the downstairs';
+use constant using_urgency    => 'descending';
 
 make_immutable;
 
