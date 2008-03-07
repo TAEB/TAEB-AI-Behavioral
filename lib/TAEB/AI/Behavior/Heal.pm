29825deb56a58f2fae27d15348dc536063d99a14
Shawn M Moore <sartak@gmail.com>
2008-03-07 12:46:55 -0500

    Have Heal cast healing if possible

--- lib/TAEB/AI/Behavior/Heal.pm
+++ lib/TAEB/AI/Behavior/Heal.pm
@@ -6,19 +6,26 @@ extends 'TAEB::AI::Behavior';
 sub prepare {
     my $self = shift;
 
-    # if we can't write Elbereth, then forget it
-    return 0 if TAEB->senses->in_wereform;
-    return 0 if TAEB->senses->is_blind;
+    return 0 if TAEB->hp * 2 > TAEB->maxhp;
 
-    if (TAEB->hp * 2 < TAEB->maxhp) {
-        $self->write_elbereth;
+    if (my $spell = TAEB->find_castable("healing")) {
+        $self->next("Z" . $spell->slot . ".");
+        $self->currently("Casting heal at myself.");
         return 100;
     }
+
+    unless (TAEB->senses->in_wereform || TAEB->senses->is_blind) {
+        $self->write_elbereth;
+        return 80;
+    }
+
+    return 0;
 }
 
 sub urgencies {
     return {
-        100 => "writing Elbereth due to low HP",
+       100 => "casting a healing spell",
+        80 => "writing Elbereth due to low HP",
     },
 }
 
