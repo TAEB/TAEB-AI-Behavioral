46143910181a73c292178ee2aff8716b5a50f7a5
Shawn M Moore <sartak@gmail.com>
2008-03-07 20:48:20 -0500

    Use the spellbook spoiler for spell information

--- lib/TAEB/Knowledge/Spell.pm
+++ lib/TAEB/Knowledge/Spell.pm
@@ -2,63 +2,6 @@
 package TAEB::Knowledge::Spell;
 use Moose;
 
-# this will disappear soon
-my %level_of = (
-    # Attack
-    'force bolt' => 1,
-    'drain life' => 2,
-    'magic missile' => 2,
-    'cone of cold' => 4,
-    'fireball' => 4,
-    'finger of death' => 7,
-
-    # Clerical
-    'protection' => 1,
-    'create monster' => 2,
-    'remove curse' => 3,
-    'create familiar' => 6,
-    'turn undead' => 6,
-
-    # Divination
-    'detect monsters' => 1,
-    'light' => 1,
-    'detect food' => 2,
-    'clairvoyance' => 3,
-    'detect unseen' => 3,
-    'identify' => 3,
-    'detect treasure' => 4,
-    'magic mapping' => 5,
-
-    # Enchantment
-    'sleep' => 1,
-    'confuse monster' => 2,
-    'slow monster' => 2,
-    'cause fear' => 3,
-    'charm monster' => 3,
-
-    # Escape
-    'jumping' => 1,
-    'haste self' => 3,
-    'invisibility' => 4,
-    'levitation' => 4,
-    'teleport away' => 6,
-
-    # Healing
-    'healing' => 1,
-    'cure blindness' => 2,
-    'cure sickness' => 3,
-    'extra healing' => 3,
-    'stone to flesh' => 3,
-    'restore ability' => 4,
-
-    # Matter
-    'knock' => 1,
-    'wizard lock' => 2,
-    'dig' => 5,
-    'polymorph' => 6,
-    'cancellation' => 7,
-);
-
 has name => (
     is       => 'rw',
     isa      => 'Str',
@@ -83,6 +26,19 @@ has slot => (
     required => 1,
 );
 
+has spoiler => (
+    is      => 'ro',
+    isa     => 'Hash::Inflator',
+    lazy    => 1,
+    handles => [qw/level read marker/],
+    default => sub {
+        my $self = shift;
+        my $name = "spellbook of " . $self->name;
+
+        Hash::Inflator->new(TAEB::Spoilers::Item::Spellbook->list->{$name});
+    },
+);
+
 =head2 castable
 
 Can this spell be cast this turn? This does not only take into account spell
@@ -94,7 +50,7 @@ sub castable {
     my $self = shift;
 
     return 0 if $self->forgotten;
-    return 0 if $level_of{ $self->name } * 5 > TAEB->power;
+    return 0 if $self->level * 5 > TAEB->power;
     return 1;
 }
 
