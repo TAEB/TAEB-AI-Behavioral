b55b60ae0232cfbe126eef11240ff7c29514be73
Shawn M Moore <sartak@gmail.com>
2008-03-08 20:46:23 -0500

    More intuitive tile type check

--- lib/TAEB/Action/Role/Direction.pm
+++ lib/TAEB/Action/Role/Direction.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package TAEB::Action::Role::Direction;
 use Moose::Role;
+use List::MoreUtils 'none';
 
 has direction => (
     is       => 'rw',
@@ -14,7 +15,7 @@ sub target_tile {
     my $self = shift;
     my $tile = TAEB->current_level->at_direction($self->direction);
 
-    if (@_ && grep { $tile->type eq $_ } @_ == 0) {
+    if (@_ && none { $tile->type eq $_ } @_) {
         TAEB->warning(blessed($self) . " can only handle tiles of type: @_");
     }
 
