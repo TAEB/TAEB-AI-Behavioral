fa789d614e4497a42caabcab9caabf8ad8b4964d
Shawn M Moore <sartak@gmail.com>
2008-03-15 13:07:08 -0500

    Remove most of the monster attributes - these will instead be lookups into Spoilers

--- lib/TAEB/World/Monster.pm
+++ lib/TAEB/World/Monster.pm
@@ -18,54 +18,7 @@ has tile => (
     weak_ref => 1,
 );
 
-has name => (
-    is  => 'rw',
-    isa => 'Str',
-);
-
-has ac => (
-    is  => 'rw',
-    isa => 'Int',
-);
-
-has mr => (
-    is  => 'rw',
-    isa => 'Int',
-);
-
-has level => (
-    is  => 'rw',
-    isa => 'Int',
-);
-
-has speed => (
-    is  => 'rw',
-    isa => 'Int',
-);
-
-has attacks => (
-    is  => 'rw',
-    isa => 'Str',
-);
-
-has resistances => (
-    is  => 'rw',
-    isa => 'Str',
-);
-
-has elbereth => (
-    is            => 'rw',
-    isa           => 'Bool',
-    default       => 1,
-    documentation => "whether the monster respects Elbereth",
-);
-
-has glyph => (
-    is  => 'rw',
-    isa => 'Str',
-);
-
-has color => (
+has type => (
     is  => 'rw',
     isa => 'Str',
 );
@@ -80,11 +33,6 @@ has tame => (
     is      => 'rw',
     isa     => 'Bool',
     default => 0,
-);
-
-has shopkeeper => (
-    is  => 'rw',
-    isa => 'Bool',
 );
 
 make_immutable;
