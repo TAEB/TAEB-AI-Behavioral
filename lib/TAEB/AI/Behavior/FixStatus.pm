15d927c6a13f24b6dacd866e7cc9f28dd630c55d
Shawn M Moore <sartak@gmail.com>
2008-03-15 12:54:10 -0500

    Refactor of FixStatus so that it's less offensive

--- lib/TAEB/AI/Behavior/FixStatus.pm
+++ lib/TAEB/AI/Behavior/FixStatus.pm
@@ -3,38 +3,81 @@ package TAEB::AI::Behavior::FixStatus;
 use TAEB::OO;
 extends 'TAEB::AI::Behavior';
 
+my @can_fix = (
+    "unicorn horn" => {
+        statuses  => [qw/blind stun conf hallu/],
+        priority  => 100,
+        action    => 'apply',
+        args      => sub { item => shift },
+        currently => sub { "Rubbing " . shift },
+    },
+    "carrot" => {
+        status    => 'blind',
+        priority  => 80,
+        action    => 'eat',
+        args      => sub { food => shift },
+    },
+    "sprig of wolfsbane" => {
+        status    => 'lycanthropy',
+        priority  => 80,
+        action    => 'eat',
+        args      => sub { food => shift },
+    },
+    "holy water" => {
+        status    => 'lycanthropy',
+        priority  => 80,
+        action    => 'quaff',
+        args      => sub { item => shift },
+    },
+);
+
 sub prepare {
     my $self = shift;
 
-    if (my @statuses = map { s/^is_//; $_ } grep { TAEB->senses->$_ } qw/is_blind is_stunned is_confused is_hallucinating/) {
-        if (my $unihorn = TAEB->find_item("unicorn horn")) {
-            $self->currently("Rubbing a unicorn horn to fix @statuses");
-            $self->do->apply(item => $unihorn);
-            return 100;
+    my %c;
+    $c{blind}       = TAEB->senses->is_blind;
+    $c{stun}        = TAEB->senses->is_stunned;
+    $c{conf}        = TAEB->senses->is_confused;
+    $c{hallu}       = TAEB->senses->is_hallucinating;
+    $c{lycanthropy} = TAEB->senses->is_lycanthropic;
+
+    for (my $i = 0; $i < @can_fix; $i += 2) {
+        my ($item_name, $fix) = @can_fix[$i, $i+1];
+
+        my @statuses = @{ $fix->{statuses} || [$fix->{status}] };
+        my @have = grep { $c{$_} } @statuses
+            or next;
+
+        my $item = TAEB->find_item($item_name)
+            or next;
+
+        my $currently;
+
+        if (ref($fix->{currently}) eq 'CODE') {
+            $currently = $fix->{currently}->($item);
         }
+        elsif ($fix->{currently}) {
+            $currently = $fix->{currently};
+        }
+        else {
+            $currently = ucfirst($fix->{action}) . " a $item";
+        }
+
+        $currently .= " to fix: " . join ', ', @have;
+
+        $self->do($fix->{action}, $fix->{args}->($item));
+        $self->currently($currently);
+
+        return $fix->{priority};
     }
 
-    if (TAEB->senses->is_blind) {
-        if (my $carrot = TAEB->find_item("carrot")) {
-            $self->currently("Eating a carrot to fix blindness");
-            $self->do->eat(food => $carrot);
-            return 80;
-        }
-    }
-
-    if (TAEB->senses->is_lycanthropic) {
-        if (my $wolfsbane = TAEB->find_item("sprig of wolfsbane")) {
-            $self->currently("Eating wolfsbane to fix lycanthropy");
-            $self->do->eat(food => $wolfsbane);
-            return 80;
-        }
-    }
+    return 0;
 }
 
 sub urgencies {
     return {
        100 => "using a unicorn horn to fix status effects",
-        80 => "eating food to fix status effects",
+        80 => "using items to fix status effects",
     },
 }
 
