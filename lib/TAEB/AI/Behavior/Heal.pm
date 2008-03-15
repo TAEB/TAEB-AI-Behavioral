28480ba6cf83a1c13ede18962502c170113f14c4
Shawn M Moore <sartak@gmail.com>
2008-03-14 21:25:33 -0500

    Cast healing "casually" - if HP < 66% and we have plenty of power.. well why not

--- lib/TAEB/AI/Behavior/Heal.pm
+++ lib/TAEB/AI/Behavior/Heal.pm
@@ -6,31 +6,42 @@ extends 'TAEB::AI::Behavior';
 sub prepare {
     my $self = shift;
 
-    return 0 if TAEB->hp * 2 > TAEB->maxhp;
+    if (TAEB->hp * 2 <= TAEB->maxhp) {
+        my $spell = TAEB->find_castable("healing")
+                || TAEB->find_castable("extra healing");
+        if ($spell) {
+            $self->do(cast => spell => $spell, direction => ".");
+            $self->currently("Casting heal at myself.");
+            return 100;
+        }
 
-    my $spell = TAEB->find_castable("healing")
-             || TAEB->find_castable("extra healing");
-    if ($spell) {
-        $self->do(cast => spell => $spell, direction => ".");
-        $self->currently("Casting heal at myself.");
-        return 100;
-    }
+        # we've probably been writing Elbereth a bunch, so find a healing potion
+        if (TAEB->hp * 3 < TAEB->maxhp) {
+            my $potion = TAEB->find_item("potion of healing")
+                    || TAEB->find_item("potion of extra healing")
+                    || TAEB->find_item("potion of full healing");
+            if ($potion) {
+                $self->do(quaff => from => $potion);
+                $self->currently("Quaffing a $potion");
+                return 90;
+            }
+        }
 
-    # we've probably been writing Elbereth a bunch, so find a healing potion
-    if (TAEB->hp * 3 < TAEB->maxhp) {
-        my $potion = TAEB->find_item("potion of healing")
-                  || TAEB->find_item("potion of extra healing")
-                  || TAEB->find_item("potion of full healing");
-        if ($potion) {
-            $self->do(quaff => from => $potion);
-            $self->currently("Quaffing a $potion");
-            return 90;
+        if (TAEB->can_elbereth) {
+            $self->write_elbereth;
+            return 80;
         }
     }
 
-    if (TAEB->can_elbereth) {
-        $self->write_elbereth;
-        return 80;
+    # now casual healing
+    if (TAEB->hp * 3 / 2 <= TAEB->maxhp) {
+        if (TAEB->power >= 20) {
+            if (my $spell = TAEB->find_castable("healing")) {
+                $self->do(cast => spell => $spell, direction => ".");
+                $self->currently("Casting heal at myself.");
+                return 100;
+            }
+        }
     }
 
     return 0;
