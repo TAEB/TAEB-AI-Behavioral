e4b09dc9554c381ee46d175d6eab7dff2b81688c
Shawn M Moore <sartak@gmail.com>
2008-03-15 10:53:10 -0500

    Add a TAEB::OO which will set the default metaclass and whatnot

---
+++ lib/TAEB/OO.pm
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+package TAEB::OO;
+use TAEB::OO;
+
+sub import {
+    my $caller = caller;
+
+    strict->import;
+    warnings->import;
+
+    return if $caller eq 'main';
+
+    Moose::init_meta($caller);
+    Moose->import({into => $caller});
+
+    return 1;
+}
+
+1;
+
