c9585ecd52a0b9b1bb05f6f65798866efc056988
Shawn M Moore <sartak@gmail.com>
2008-03-14 21:39:41 -0500

    Add a FixStatus behavior which will use a unicorn horn, eat a carrot, or eat wolfsbane

---
+++ lib/TAEB/AI/Behavior/FixStatus.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+package TAEB::AI::Behavior::FixStatus;
+use Moose;
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+
+    if (my @statuses = map { s/^is_//; $_ } grep { $self->senses->$_ } qw/is_blind is_stunned is_confused is_hallucinating/) {
+        if (my $unihorn = TAEB->find_item("unicorn horn")) {
+            $self->currently("Rubbing a unicorn horn to fix @statuses");
+            $self->do->apply(item => $unihorn);
+            return 100;
+        }
+    }
+
+    if ($self->senses->is_blind) {
+        if (my $carrot = TAEB->find_item("carrot")) {
+            $self->currently("Eating a carrot to fix blindness");
+            $self->do->eat(food => $carrot);
+            return 80;
+        }
+    }
+
+    if ($self->senses->is_lycanthropic) {
+        if (my $wolfsbane = TAEB->find_item("sprig of wolfsbane")) {
+            $self->currently("Eating wolfsbane to fix lycanthropy");
+            $self->do->eat(food => $wolfsbane);
+            return 80;
+        }
+    }
+}
+
+sub urgencies {
+    return {
+       100 => "using a unicorn horn to fix status effects",
+        80 => "eating food to fix status effects",
+    },
+}
+
+1;
+
