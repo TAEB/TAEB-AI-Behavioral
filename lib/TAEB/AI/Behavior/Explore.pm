9782381976270a9ea47eb244ae6e22ad7f656cf8
Shawn M Moore <sartak@gmail.com>
2008-03-15 19:31:32 -0500

    Try to avoid the explore dijkstra if we tried exploring and got no path (until we get a kickstart letting us know to explore again)

--- lib/TAEB/AI/Behavior/Explore.pm
+++ lib/TAEB/AI/Behavior/Explore.pm
@@ -3,8 +3,15 @@ package TAEB::AI::Behavior::Explore;
 use TAEB::OO;
 extends 'TAEB::AI::Behavior';
 
+has done_exploring => (
+    isa     => 'Bool',
+    default => 0,
+);
+
 sub prepare {
     my $self = shift;
+
+    return 0 if $self->done_exploring;
 
     my $path = TAEB::World::Path->first_match(
         sub {
@@ -13,6 +20,8 @@ sub prepare {
         },
     );
 
+    $self->done_exploring(1) if !$path;
+
     $self->if_path($path, "Exploring", 100);
 }
 
@@ -20,6 +29,16 @@ sub urgencies {
     return {
         100 => "path to an unexplored square",
     },
+}
+
+sub msg_tile_update {
+    my $self = shift;
+    $self->done_exploring(0);
+}
+
+sub msg_dlvl_change {
+    my $self = shift;
+    $self->done_exploring(0);
 }
 
 make_immutable;
