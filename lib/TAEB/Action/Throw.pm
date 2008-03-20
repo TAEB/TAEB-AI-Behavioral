b42541dd554c5732c166caea5c549352befac287
Shawn M Moore <sartak@gmail.com>
2008-03-20 01:09:22 -0500

    Make throw respond to missing_item

--- lib/TAEB/Action/Throw.pm
+++ lib/TAEB/Action/Throw.pm
@@ -38,6 +38,14 @@ sub msg_throw_count {
     TAEB->inventory->decrease_quantity($self->item->slot, $count - 1);
 }
 
+sub respond_missing_item {
+    my $self = shift;
+    TAEB->debug("We don't have item " . $self->item . ", escaping.");
+    TAEB->inventory->remove($self->food->slot);
+    $self->aborted(1);
+    return "\e";
+}
+
 make_immutable;
 no Moose;
 
