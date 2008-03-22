b7c4cdca23ad69b611dd26a832d4d267bd85ff2a
Shawn M Moore <sartak@gmail.com>
2008-03-22 01:02:50 -0500

    Engrave subscribes to wand identify messages now

--- lib/TAEB/Action/Engrave.pm
+++ lib/TAEB/Action/Engrave.pm
@@ -15,6 +15,11 @@ has text => (
     default => 'Elbereth',
 );
 
+has got_identifying_message => (
+    isa     => 'Bool',
+    default => 0,
+);
+
 sub engrave_slot {
     my $self = shift;
     my $engraver = $self->implement;
@@ -26,6 +31,20 @@ sub engrave_slot {
 sub respond_write_with    { shift->engrave_slot }
 sub respond_write_what    { shift->text . "\n" }
 sub respond_add_engraving { 'y' }
+
+sub msg_wand {
+    my $self = shift;
+    $self->got_identifying_message(1);
+    $self->implement->rule_out_all_but(@_);
+}
+
+sub done {
+    my $self = shift;
+    return unless blessed $self->implement;
+    return if $self->got_identifying_message;
+    return if $self->implement->identity; # perhaps we identified it?
+    $self->implement->possibility_tracker->no_engrave_message;
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
