7d6dabf3946ead9fddd92927f35adf3860f05dd4
Shawn M Moore <sartak@gmail.com>
2008-03-22 13:07:05 -0500

    Don't pick up items that are in shops

--- lib/TAEB/AI/Behavior/GetItems.pm
+++ lib/TAEB/AI/Behavior/GetItems.pm
@@ -7,17 +7,20 @@ use List::MoreUtils 'any';
 sub prepare {
     my $self = shift;
 
-    my @want = grep { TAEB->want_item($_) } TAEB->current_tile->items;
-    if (@want) {
-        TAEB->debug("TAEB wants items! @want");
-        $self->currently("Picking up items");
-        $self->do("pickup");
-        return 100;
+    unless (TAEB->current_tile->in_shop) {
+        my @want = grep { TAEB->want_item($_) } TAEB->current_tile->items;
+        if (@want) {
+            TAEB->debug("TAEB wants items! @want");
+            $self->currently("Picking up items");
+            $self->do("pickup");
+            return 100;
+        }
     }
 
     my $path = TAEB::World::Path->first_match(sub {
         my $tile = shift;
         $tile->is_walkable or return;
+        return if $tile->in_shop;
 
         return 1 if $tile->might_have_new_item;
         return any { TAEB->want_item($_) } $tile->items;
