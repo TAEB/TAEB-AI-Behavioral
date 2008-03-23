d04279ae99f9d18dc6fb87c9594fd08934d10b25
jluehrs2 <jluehrs2@uiuc.edu>
2008-03-23 06:29:14 -0500

    add an Equip behavior for changing out taeb's equipment... right now it only handles weapons, and is pretty broken... don't use it yet!

---
+++ lib/TAEB/AI/Behavior/Equip.pm
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+package TAEB::AI::Behavior::Equip;
+use TAEB::OO;
+use TAEB::Util 'avg_dice';
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+    my $current_weapon = TAEB->inventory->wielded;
+    my $current_avgdam = avg_dice($current_weapon->sdam);
+
+    my ($best_weapon, $best_avgdam) = ($current_weapon, $current_avgdam);
+    TAEB->inventory->each(sub {
+        return if $_->buc eq 'cursed';
+        my $avgdam = avg_dice($_->sdam);
+        if ($avgdam > $best_avgdam) {
+            $best_avgdam = $avgdam;
+            $best_weapon = $_;
+        }
+    }, class => 'weapon');
+    return 0 if $best_weapon->slot eq $current_weapon->slot;
+
+    $self->do(wield => weapon => $best_weapon);
+    $self->currently("Equipping a better weapon");
+    return 100;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
