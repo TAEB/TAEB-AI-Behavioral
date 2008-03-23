9e7025994c83d6cc1583f3a5be01f1fc726ef4db
Shawn M Moore <sartak@gmail.com>
2008-03-22 21:27:34 -0500

    Monster->is_shk (which is not 100% right but close enough for now)

--- lib/TAEB/World/Monster.pm
+++ lib/TAEB/World/Monster.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package TAEB::World::Monster;
 use TAEB::OO;
+use TAEB::Util qw/:colors/;
 
 has glyph => (
     isa      => 'Str',
@@ -16,6 +17,11 @@ has tile => (
     isa      => 'TAEB::World::Tile',
     weak_ref => 1,
 );
+
+sub is_shk {
+    my $self = shift;
+    $self->glyph eq '@' && $self->color eq COLOR_WHITE;
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
