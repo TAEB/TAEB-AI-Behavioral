597f8c330a9b600f1d59a851974d30f9696c9f0b
Shawn M Moore <sartak@gmail.com>
2008-03-22 19:54:38 -0500

    If our debt is still undefined by the time the $ command is through, we know we owe nothing

--- lib/TAEB/Action/Gold.pm
+++ lib/TAEB/Action/Gold.pm
@@ -5,6 +5,14 @@ extends 'TAEB::Action';
 
 use constant command => '$';
 
+# if we have debt, then we'll get a message. otherwise, we have no debt, so
+# we're good
+sub done {
+    if (!defined(TAEB->senses->debt)) {
+        TAEB->senses->debt(0);
+    }
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
