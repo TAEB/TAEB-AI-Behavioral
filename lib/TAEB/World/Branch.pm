8b6c7df19d26eab56632e0521bcd236c9672fdaf
Sean Kelly <kelly530@umn.edu>
2008-03-25 17:04:53 -0500

    Provide a boring overload for branches

--- lib/TAEB/World/Branch.pm
+++ lib/TAEB/World/Branch.pm
@@ -2,6 +2,14 @@
 package TAEB::World::Branch;
 use TAEB::OO;
 
+use overload
+    q{""} => sub {
+        my $self = shift;
+        sprintf "[%s: name=%s]",
+            $self->meta->name,
+            $self->name;
+    };
+
 has name => (
     isa      => 'Str',
     required => 1,
