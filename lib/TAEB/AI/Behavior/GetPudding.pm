00860469b95bac0526520f76891329b5c56de258
Shawn M Moore <sartak@gmail.com>
2008-03-25 01:53:27 -0500

    GetPudding behavior *cough*

---
+++ lib/TAEB/AI/Behavior/GetPudding.pm
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+package TAEB::AI::Behavior::GetPudding;
+use TAEB::OO;
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+
+    return 0 unless TAEB->senses->can_kick;
+
+    my ($sink, $dir);
+    my $check = sub {
+        my ($tile, $d) = @_;
+        return ($sink, $dir) = ($tile, $d)
+            if $tile->type eq 'sink'
+            && $tile->glyph eq '{' # no items or monsters on it
+            && !$tile->got_pudding;
+
+        return 0;
+    };
+
+    if (TAEB->any_adjacent($check)) {
+        $self->do(kick => direction => $dir);
+        $self->currently("Kicking a sink for a pudding");
+        return 100;
+    }
+
+    my $path = TAEB::World::Path->first_match($check);
+    $self->if_path($path => "Heading towards a sink");
+}
+
+sub urgencies {
+    return {
+        100 => "kicking an adjacent sink",
+         50 => "path to a sink",
+    },
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
