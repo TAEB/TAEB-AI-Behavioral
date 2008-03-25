2f9e4a3ba7082ff723c3ac3bb93cefbea955de0b
bagofholding <bagofholding@gmail.com>
2008-03-24 20:57:56 -0500

    fix gray stone bug

--- lib/TAEB/Spoilers/Item/Gem.pm
+++ lib/TAEB/Spoilers/Item/Gem.pm
@@ -267,8 +267,8 @@ has multi_identity_appearances => (
     isa        => 'ArrayRef',
     auto_deref => 1,
     default    => sub {
-        [map { "$_ gem" } (qw/white red blue orange black green yellow violet/,
-                           'yellowish brown')]; #XXX: , 'gray stone'];
+        [(map { "$_ gem" } (qw/white red blue orange black green yellow violet/,
+                           'yellowish brown')), 'gray stone'];
     },
 );
 
