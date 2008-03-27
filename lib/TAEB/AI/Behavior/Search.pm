1e1915805c74210ba322ab09f77de46b58f31970
Shawn M Moore <sartak@gmail.com>
2008-03-27 00:23:01 -0500

    Replace the Search code with one that takes into account Tile->searchability. Needs tweaking though

--- lib/TAEB/AI/Behavior/Search.pm
+++ lib/TAEB/AI/Behavior/Search.pm
@@ -9,29 +9,23 @@ sub prepare {
     my $path = TAEB::World::Path->max_match(
         sub {
             my ($tile, $path) = @_;
-
-            # search walls and solid rock
-            return undef unless $tile->type eq 'wall' || $tile->type eq 'rock';
-            return 1 / (($tile->searched + length $path) || 1);
+            $tile->searchability / (length($path) || 1);
         },
     );
 
-    # are we adjacent to an unsearched wall? if so, begin searching
-    if (length($path->path) <= 1) {
-        $self->currently("Searching.");
+    if ($path && $path->path eq '') {
+        $self->currently("Searching adjacent walls and rock");
         $self->do('search');
         return 100;
     }
 
-    $self->currently("Heading to a search hotspot");
-    $self->do(move => path => $path);
-    return 50;
+    $self->if_path($path => "Heading to a search hotspot");
 }
 
 sub urgencies {
     return {
-        100 => "path to an unsearched wall",
-         50 => "searching an adjacent unsearched wall",
+        100 => "searching adjacent walls and rock",
+         50 => "path to an unsearched wall",
     }
 }
 
