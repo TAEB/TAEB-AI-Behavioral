98a2034f4839f04b6f77c3e4b62d35c988ddedf9
jluehrs2 <jluehrs2@uiuc.edu>
2008-03-27 18:16:28 -0500

    ...oops

--- lib/TAEB/Action/Engrave.pm
+++ lib/TAEB/Action/Engrave.pm
@@ -45,7 +45,7 @@ sub msg_wand {
 
 sub done {
     my $self = shift;
-    $self->engraving($self->engraving . $self->text);
+    TAEB->current_tile->engraving(TAEB->current_tile->engraving . $self->text);
     return unless blessed $self->item;
 
     if ($self->item->class eq 'wand') {
