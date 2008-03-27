f1c778cf7ce5c19abb38d88e31dc00224391d711
TAEB <TAEB@spam.mathemaniac.org>
2008-03-27 00:51:43 -0500

    New behavior: CurseCheck

---
+++ lib/TAEB/AI/Behavior/CurseCheck.pm
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+package TAEB::AI::Behavior::CurseCheck;
+use TAEB::OO;
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+
+    # Can't see altar flash when blind
+    if (TAEB->senses->is_blind)
+        return 0;
+
+    my @drop = grep { $_->buc eq 'unknown' } TAEB->inventory->items;
+
+    # No point in cursechecking no items
+    return 0 unless @drop;
+
+    if (TAEB->current_tile->type eq 'altar') {
+        $self->currently("Dropping items for cursechecking.");
+        $self->do(drop => items => \@drop);
+        return 100;
+    }
+
+    # find an altar to cursecheck on
+    my $path = TAEB::World::Path->first_match(
+        sub { shift->type eq 'altar' },
+    );
+
+    $self->if_path($path => "Heading towards an altar");
+}
+
+sub drop {
+    my $self = shift;
+    my $item = shift;
+
+    return if ((TAEB->senses->is_blind) ||
+               ($item->buc eq 'unknown') ||
+               (TAEB->current_tile->type ne 'altar'));
+
+    TAEB->debug("Yes, I want to drop $item because it needs to be cursechecked.");
+    return 1;
+}
+
+sub urgencies {
+    return {
+        100 => "cursechecking at an altar",
+         50 => "locating altar",
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
