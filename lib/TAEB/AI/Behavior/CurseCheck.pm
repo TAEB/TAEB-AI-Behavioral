271bd0ee1c1127d264c96d76b623fa1e1cd3845c
TAEB <TAEB@spam.mathemaniac.org>
2008-03-28 13:34:53 -0500

    backtrack to altar on other level to cursecheck

--- lib/TAEB/AI/Behavior/CurseCheck.pm
+++ lib/TAEB/AI/Behavior/CurseCheck.pm
@@ -8,7 +8,9 @@ sub prepare {
 
     # Can't see altar flash when blind
     return 0 if TAEB->senses->is_blind;
-    return 0 unless TAEB->current_level->has_type('altar');
+
+    my $level = TAEB->nearest_level(sub { shift->has_type('altar') })
+        or return 0;
 
     my @drop = grep { $_->buc eq 'unknown' && $_->appearance ne 'gold piece' } TAEB->inventory->items;
 
@@ -24,6 +26,7 @@ sub prepare {
     # find an altar to cursecheck on
     my $path = TAEB::World::Path->first_match(
         sub { shift->type eq 'altar' },
+        on_level => $level,
     );
 
     $self->if_path($path => "Heading towards an altar");
