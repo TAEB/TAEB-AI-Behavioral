95eb9fa8dd521fec15c3b57e556b945139a8eeea
jluehrs2 <jluehrs2@uiuc.edu>
2008-03-28 11:30:52 -0500

    if there are no enemies left in sight and taeb's hp is less than 75% full, then rest up a bit more, to keep him from wandering around with low hp

--- lib/TAEB/AI/Behavior/Defend.pm
+++ lib/TAEB/AI/Behavior/Defend.pm
@@ -5,44 +5,50 @@ extends 'TAEB::AI::Behavior';
 
 sub prepare {
     my $self = shift;
-    return 0 if TAEB->hp * 2 > TAEB->maxhp;
 
-    my $can_engrave = TAEB->can_engrave;
-    my $elbereths   = TAEB->senses->elbereth_count;
+    if (TAEB->hp * 2 <= TAEB->maxhp) {
+        my $can_engrave = TAEB->can_engrave;
+        my $elbereths   = TAEB->senses->elbereth_count;
 
-    my ($adjacent_ignoring, $adjacent_respecting) = (0, 0);
-    TAEB->each_adjacent(sub {
-        my $monster = shift->monster or return;
-        return unless $monster->is_enemy;
+        my ($adjacent_ignoring, $adjacent_respecting) = (0, 0);
+        TAEB->each_adjacent(sub {
+            my $monster = shift->monster or return;
+            return unless $monster->is_enemy;
 
-        $monster->respects_elbereth
-            ? ++$adjacent_respecting
-            : ++$adjacent_ignoring
-    });
+            $monster->respects_elbereth
+                ? ++$adjacent_respecting
+                : ++$adjacent_ignoring
+        });
 
-    # if there's an adjacent monster that ignores Elbereth, then we only write
-    # Elbereth if there's no Elbereth on the ground AND there's an adjacent
-    # Elbereth-respecting monster. we don't rest on Elbereth
-    if ($adjacent_ignoring) {
-        if ($can_engrave && $adjacent_respecting && $elbereths == 0) {
+        # if there's an adjacent monster that ignores Elbereth, then we only
+        # write Elbereth if there's no Elbereth on the ground AND there's an
+        # adjacent Elbereth-respecting monster. we don't rest on Elbereth
+        if ($adjacent_ignoring) {
+            if ($can_engrave && $adjacent_respecting && $elbereths == 0) {
+                $self->write_elbereth;
+                return 100;
+            }
+            return 0;
+        }
+
+        # otherwise, we write Elbereth if we can and there's not already an
+        # excessive amount of them
+        if ($can_engrave && $elbereths < 3) {
             $self->write_elbereth;
             return 100;
         }
-        return 0;
-    }
 
-    # otherwise, we write Elbereth if we can and there's not already an
-    # excessive amount of them
-    if ($can_engrave && $elbereths < 3) {
-        $self->write_elbereth;
-        return 100;
+        # finally, we have an Elbereth under us, so we rest up to heal
+        if ($elbereths) {
+            $self->currently("Resting on an Elbereth tile.");
+            $self->do('search');
+            return 80;
+        }
     }
-
-    # finally, we have an Elbereth under us, so we rest up to heal
-    if ($elbereths) {
-        $self->currently("Resting on an Elbereth tile.");
+    if (TAEB->hp * 4 <= TAEB->maxhp * 3 && !TAEB->current_level->has_enemies) {
+        $self->currently("Resting up to gain some hp");
         $self->do('search');
-        return 80;
+        return 50;
     }
 }
 
@@ -50,6 +56,7 @@ sub urgencies {
     return {
        100 => "writing Elbereth due to low HP",
        80  => "resting on an Elbereth tile",
+       50  => "resting to regain hp before continuing",
     },
 }
 
