90e636d9a9b2f921470add85c83fd5825a5673fe
bagofholding <bagofholding@gmail.com>
2008-03-30 00:23:11 -0500

    Gah, forgot to add in the semicolon after commenting

--- lib/TAEB/AI/Behavior/AcquireMithril.pm
+++ lib/TAEB/AI/Behavior/AcquireMithril.pm
@@ -13,8 +13,12 @@ sub prepare {
     # Yeah, so I'm lazy.
     grep { TAEB->role eq $_ } qw/Hea Tou Val/ or return 0;
 
-    return 0 if $item->is_wearing
-             || $item->buc eq 'cursed';
+    # Call me picky if you must.
+    # At this point even cursed mithril will result in good AC
+    # and even MC3.  I'm not as picky -- sawtooth
+    return 0 if $item->is_wearing # ||
+    #            $item->buc eq "unknown" ||
+    #            $item->buc eq "cursed";
 
     $self->currently("Putting on mithril.");
     $self->do(wear => item => $item);
