45a136d4384bac2582d46e52bd66bd0e2e68f715
TAEB <TAEB@spam.mathemaniac.org>
2008-03-31 11:59:04 -0500

    add infrastructure to process ring-identifying messages

--- lib/TAEB/Action/Drop.pm
+++ lib/TAEB/Action/Drop.pm
@@ -5,7 +5,18 @@ extends 'TAEB::Action';
 
 use constant command => "Da\n";
 
+has got_identifying_message => (
+    isa     => 'Bool',
+    default => 0,
+);
+
 # logic is elsewhere sadly
+
+sub msg_ring {
+    my $self = shift;
+    $self->got_identifying_message(1);
+    $self->item->rule_out_all_but(@_);
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
