0d0a4fbcae3fec49dbda11778482ec40563e2f0e
TAEB <TAEB@spam.mathemaniac.org>
2008-04-01 23:00:15 -0500

    If we are on the Oracle level, then we are in the dungeons branch.

--- lib/TAEB/World/Level.pm
+++ lib/TAEB/World/Level.pm
@@ -494,6 +494,8 @@ sub msg_dungeon_level {
     TAEB->info("Hey, I know this level! It's $level!")
         if !$self->$islevel;
 
+    $self->branch = 'dungeons' if $level eq 'oracle';
+
     $self->$islevel(1);
 }
 
