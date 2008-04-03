05bf15d4b2a9720ee84457db45e83855873ce7cb
Shawn M Moore <sartak@gmail.com>
2008-04-03 16:55:06 -0500

    Display branch stairs as yellow

--- lib/TAEB/World/Tile/Stairs.pm
+++ lib/TAEB/World/Tile/Stairs.pm
@@ -2,6 +2,7 @@
 package TAEB::World::Tile::Stairs;
 use TAEB::OO;
 extends 'TAEB::World::Tile';
+use TAEB::Util ':colors';
 
 has other_side => (
     isa      => 'TAEB::World::Tile',
@@ -9,6 +10,23 @@ has other_side => (
 );
 
 sub traverse_command { shift->floor_glyph }
+
+around draw_debug => sub {
+    my $orig           = shift;
+    my $self           = shift;
+    my $display_method = shift;
+
+    my $different_branch = $self->other_side
+                        && $self->other_side->branch
+                        && $self->branch
+                        && $self->branch ne $self->other_side->branch;
+
+    if ($different_branch) {
+        return Curses::addch(Curses::A_BOLD | Curses::COLOR_PAIR(COLOR_BROWN) | ord $self->$display_method);
+    }
+
+    $self->$orig($display_method, @_);
+};
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
