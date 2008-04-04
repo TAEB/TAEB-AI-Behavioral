58034bc9cb27c10b3b8582e92b2843209364783e
Shawn M Moore <sartak@gmail.com>
2008-04-04 06:25:46 -0500

    Rewrite the output code in Hivemind to use Template::Declare::Anon

--- lib/TAEB/AI/Personality/Hivemind.pm
+++ lib/TAEB/AI/Personality/Hivemind.pm
@@ -1,24 +1,70 @@
 #!/usr/bin/env perl
 package TAEB::AI::Personality::Hivemind;
+BEGIN {
+    use Template::Declare::Tags;
+    delete ${TAEB::AI::Personality::Hivemind::}{meta}
+}
 use TAEB::OO;
+use Template::Declare::Anon;
 extends 'TAEB::AI::Personality';
 
 sub next_action {
-    $main::request->print('<html><body>');
-    $main::request->print('<form><input id="cmd" type="text" size="1" maxlength="1" name="c"></input></form><script>document.getElementById("cmd").focus()</script>');
-    $main::request->print('<pre>' . TAEB->vt->as_string("\n") . '</pre>');
-    $main::request->print('</body></html>');
+    $main::request->print(anon_template {
+        html {
+            body {
+                form {
+                    input {
+                        attr {
+                            id        => "cmd",
+                            type      => "text",
+                            size      => 1,
+                            maxlength => 1,
+                            name      => "c",
+                        }
+                    }
+                }
+                script {
+                    outs_raw 'document.getElementById("cmd").focus()';
+                }
+                pre {
+                    TAEB->vt->as_string("\n");
+                }
+            }
+        }
+    });
+
     $main::request->next;
     my $c = substr($main::request->param('c'), 0, 1);
     return TAEB::Action->new_action(custom => string => $c);
 }
 
 sub respond {
-    $main::request->print('<html><body>');
-    $main::request->print('<pre>' . TAEB->topline . '</pre>');
-    $main::request->print('<form><input id="cmd" type="text" size="20" name="c"></input></form><script>document.getElementById("cmd").focus()</script>');
-    $main::request->print('<pre>' . TAEB->vt->as_string("\n", 1) . '</pre>');
-    $main::request->print('</body></html>');
+    $main::request->print(anon_template {
+        html {
+            body {
+                pre {
+                    TAEB->topline;
+                }
+                form {
+                    input {
+                        attr {
+                            id        => "cmd",
+                            type      => "text",
+                            size      => 20,
+                            name      => "c",
+                        }
+                    }
+                }
+                script {
+                    outs_raw 'document.getElementById("cmd").focus()';
+                }
+                pre {
+                    TAEB->vt->as_string("\n", 1);
+                }
+            }
+        }
+    });
+
     $main::request->next;
     return $main::request->param('c');
 }
