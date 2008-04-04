40c1d582f299530aa4ac714fc3edf36f9ae860e9
Shawn M Moore <sartak@gmail.com>
2008-04-04 01:05:18 -0500

    Refactor Descend to vastly prefer the main dungeon over the mine

--- lib/TAEB/AI/Behavior/Descend.pm
+++ lib/TAEB/AI/Behavior/Descend.pm
@@ -3,11 +3,38 @@ package TAEB::AI::Behavior::Descend;
 use TAEB::OO;
 extends 'TAEB::AI::Behavior::GotoTile';
 
-sub match_tile {
-    $_[1]->floor_glyph eq '>' ? (['descend'], 'Descending') : undef
+has stairsdown => (
+    isa        => 'ArrayRef[TAEB::World::Tile]',
+    auto_deref => 1,
+    default    => sub { [] },
+);
+
+sub correct_stairs {
+    my $self = shift;
+    my $tile = shift;
+
+    # not stairs down!
+    return 0 unless $tile->floor_glyph eq '>';
+
+    # we only have one choice
+    return 1 if @{ $self->stairsdown } == 1;
+
+    # here we have multiple choices and this one is the mines. no thank you
+    return 0 $tile->other_side && $tile->other_side->branch eq 'mines';
+
+    return 1;
 }
 
-sub first_pass { TAEB->current_level->has_type('stairsdown') }
+sub match_tile {
+    my ($self, $tile) = @_;
+    $self->correct_stairs($tile) ? (['descend'], 'Descending') : undef
+}
+
+sub first_pass {
+    my @stairsdown = TAEB->current_level->has_type('stairsdown');
+    shift->stairsdown(\@stairsdown);
+    return @stairsdown;
+}
 
 use constant tile_description => 'the downstairs';
 use constant using_urgency    => 'descending';
