6ab17d1f28808b6f453f4101d5fbdd89e3a74e08
Sean Kelly <kelly530@umn.edu>
2008-04-04 12:01:58 -0500

    Calculate the failure rate for a spell: does not entirely work

--- lib/TAEB/Knowledge/Spell.pm
+++ lib/TAEB/Knowledge/Spell.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package TAEB::Knowledge::Spell;
 use TAEB::OO;
+use List::Util qw/max min/;
 
 use overload %TAEB::Meta::Overload::default;
 
@@ -54,6 +55,54 @@ sub castable {
     return 0 if TAEB->nutrition <= 10 && $self->name ne 'detect food';
 
     return 1;
+}
+
+sub failure_rate {
+    my $self = shift;
+    my %penalties = (
+        # role base emergency shield suit int/wis
+        Arc => [ 5,   0,  2,  10, 'int' ],
+        Bar => [ 14,  0,  0,  8,  'int' ],
+        Cav => [ 12,  0,  1,  8,  'int' ],
+        Hea => [ 3,  -3,  2,  10, 'wis' ],
+        Kni => [ 8,  -2,  0,  9,  'wis' ],
+        Mon => [ 8,  -2,  2,  20, 'wis' ],
+        Pri => [ 3,  -3,  2,  10, 'wis' ],
+        Ran => [ 9,   2,  1,  10, 'int' ],
+        Rog => [ 8,   0,  1,  9,  'int' ],
+        Sam => [ 10,  0,  0,  8,  'int' ],
+        Tou => [ 5,   1,  2,  10, 'int' ],
+        Val => [ 10, -2,  0,  9,  'wis' ],
+        Wiz => [ 1,   0,  3,  10, 'int' ],
+    );
+
+    my $penalty = $penalties{TAEB->role}->[0];
+
+    # this is where inventory penalty calculation would go
+
+    $penalty += $penalties{TAEB->role}->[1] if $self->emergency;
+    $penalty -= 4 if $self->role eq TAEB->role;
+
+    my $chance;
+    my $SKILL = 0; # XXX: this needs to reference skill levels
+    my $basechance = int(TAEB->($penalties{TAEB->role}->[4]) * 11 / 2);
+    my $diff = (($self->level - 1) * 4 - ($SKILL * 6 + int(TAEB->xl / 3) + 1));
+    if ($diff > 0) {
+        $chance = $basechance - int(sqrt(900 * $diff + 2000));
+    }
+    else {
+        my $learning = int(((-15) * $diff) / $SKILL);
+        $chance = $basechance + min($learning, 20);
+    }
+
+    $chance = max(min($chance, 120), 0);
+
+    # shield and special spell
+
+    $chance = int($chance * (20 - $penalty) / 15) - $penalty;
+    $chance = max(min($chance, 100), 0);
+
+    return $chance;
 }
 
 sub forgotten {
