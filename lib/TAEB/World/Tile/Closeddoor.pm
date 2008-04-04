f3083ee36fd9e5df54cc2775c22b44095fbc96f9
Shawn M Moore <sartak@gmail.com>
2008-04-04 00:25:27 -0500

    Make doors colorful in draw_debug

--- lib/TAEB/World/Tile/Closeddoor.pm
+++ lib/TAEB/World/Tile/Closeddoor.pm
@@ -2,6 +2,7 @@
 package TAEB::World::Tile::Closeddoor;
 use TAEB::OO;
 extends 'TAEB::World::Tile';
+use TAEB::Util ':colors';
 
 has locked => (
     isa     => 'DoorState',
@@ -16,6 +17,24 @@ has is_shop => (
     isa     => 'Bool',
     default => 0,
 );
+
+around draw_debug => sub {
+    my $orig           = shift;
+    my $self           = shift;
+    my $display_method = shift;
+
+    if ($self->is_shop) {
+        return Curses::addch(Curses::A_BOLD | Curses::COLOR_PAIR(COLOR_RED) | ord $self->$display_method);
+    }
+    elsif ($self->locked eq 'locked') {
+        return Curses::addch(Curses::A_BOLD | Curses::COLOR_PAIR(COLOR_BROWN) | ord $self->$display_method);
+    }
+    elsif ($self->locked eq 'closed') {
+        return Curses::addch(Curses::A_BOLD | Curses::COLOR_PAIR(COLOR_GREEN) | ord $self->$display_method);
+    }
+
+    $self->$orig($display_method, @_);
+};
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
