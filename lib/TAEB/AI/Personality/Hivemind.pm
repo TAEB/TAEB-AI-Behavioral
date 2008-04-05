24e88ddadcbdf85c47bf63551312604fdc9ded11
Shawn M Moore <sartak@gmail.com>
2008-04-05 08:15:04 -0500

    Hivemind->fill_action which does a second cede for the arguments to the action

--- lib/TAEB/AI/Personality/Hivemind.pm
+++ lib/TAEB/AI/Personality/Hivemind.pm
@@ -17,6 +17,26 @@ sub respond {
     $main::request->param('c');
 }
 
+sub fill_action {
+    my $self        = shift;
+    my $name        = shift;
+    my $pkg         = "TAEB::Action::$name";
+    my $action_meta = $pkg->meta;
+    my @required    = grep { $_->provided }
+                      $action_meta->compute_all_applicable_attributes;
+    return $pkg->new if !@required;
+
+    $main::request->print(TAEB::AI::Personality::Hivemind::Templates->action_arguments(@required));
+    $main::request->next;
+
+    my %args;
+    for my $attr (@required) {
+        $args{$attr->name} = $main::request->param($attr->name);
+    }
+
+    $pkg->new(%args);
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
