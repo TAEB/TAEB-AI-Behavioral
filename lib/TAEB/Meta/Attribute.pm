ddbbcd4a060f5d1b0622e9123bcaee67a5339efe
Shawn M Moore <sartak@gmail.com>
2008-04-05 08:10:43 -0500

    Override Moose's clone_and_inherit_options to ignore what we override, we want full control (specifically, over "provided")

--- lib/TAEB/Meta/Attribute.pm
+++ lib/TAEB/Meta/Attribute.pm
@@ -22,6 +22,29 @@ around _process_options => sub {
     $orig->(@_);
 };
 
+sub clone_and_inherit_options {
+    my ($self, %options) = @_;
+
+    if ($options{isa}) {
+        my $type_constraint;
+        if (blessed($options{isa}) && $options{isa}->isa('Moose::Meta::TypeConstraint')) {
+            $type_constraint = $options{isa};
+        }
+        else {
+            $type_constraint = Moose::Util::TypeConstraints::find_or_create_type_constraint(
+                $options{isa}
+            );
+            (defined $type_constraint)
+                || confess "Could not find the type constraint '" . $options{isa} . "'";
+        }
+        $options{type_constraint} = $type_constraint;
+        delete $options{isa};
+    }
+
+
+    $self->clone(%options);
+}
+
 no Moose;
 
 1;
