a88788c3fed3f88fc58989639815e2c2cf1204ab
Sebastian Paaske Tørholm <TAEB@spam.mathemaniac.org>
2008-04-15 09:52:42 -0500

    Add 'in_zoo' to tiles

--- lib/TAEB/World/Tile.pm
+++ lib/TAEB/World/Tile.pm
@@ -135,6 +135,12 @@ has in_vault => (
     documentation => "Is this tile inside a vault?",
 );
 
+has in_zoo => (
+    isa           => 'Bool',
+    default       => 0,
+    documentation => "Is this tile inside a zoo?",
+);
+
 =head2 basic_cost -> Int
 
 This returns the basic cost of entering a tile. It's not very smart, but it
