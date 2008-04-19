5262bcf02860e75056157706a4ea716604831f7f
Sebastian Paaske Tørholm <TAEB@spam.mathemaniac.org>
2008-04-19 17:48:46 -0500

    Don't try to go to a monster we see through warning.

--- lib/TAEB/AI/Behavior/Melee.pm
+++ lib/TAEB/AI/Behavior/Melee.pm
@@ -30,7 +30,9 @@ sub prepare {
     my $path = TAEB::World::Path->first_match(
         sub {
             my $tile = shift;
-            $tile->has_enemy && $tile->monster->is_meleeable
+            $tile->has_enemy &&
+            $tile->monster->is_meleeable &&
+           !$tile->monster->is_seen_through_warning
         },
         through_unknown => 1,
         include_endpoints => 1,
