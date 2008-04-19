a598c34917af376eac4a0659debceec3023f251d
jluehrs2 <jluehrs2@uiuc.edu>
2008-04-19 18:40:50 -0500

    update Equip to use the combat spoiler

--- lib/TAEB/AI/Behavior/Equip.pm
+++ lib/TAEB/AI/Behavior/Equip.pm
@@ -1,24 +1,30 @@
 #!/usr/bin/perl
 package TAEB::AI::Behavior::Equip;
 use TAEB::OO;
-use TAEB::Util 'dice';
+use TAEB::Spoilers::Combat;
 extends 'TAEB::AI::Behavior';
 
 sub prepare {
     my $self = shift;
-    my $current_weapon = TAEB->inventory->wielded;
-    my $current_avgdam = dice($current_weapon->sdam);
+    my $current_weapon = TAEB->inventory->wielded || '-';
+    my $current_avgdam = TAEB::Spoilers::Combat::damage($current_weapon);
 
     my ($best_weapon, $best_avgdam) = ($current_weapon, $current_avgdam);
     TAEB->inventory->each(sub {
         return if $_->match(buc => ['cursed', undef]);
-        my $avgdam = dice($_->sdam);
+        my $avgdam = TAEB::Spoilers::Combat::damage($_);
         if ($avgdam > $best_avgdam) {
             $best_avgdam = $avgdam;
             $best_weapon = $_;
         }
     }, class => 'weapon');
-    return 0 if $best_weapon->match(slot => $current_weapon->slot);
+    # XXX: need to handle switching from barehanded to weapon and back
+    if (blessed $best_weapon) {
+        return 0 if $best_weapon->match(slot => $current_weapon->slot);
+    }
+    else {
+        return 0 if $best_weapon eq $current_weapon;
+    }
 
     $self->do(wield => weapon => $best_weapon);
     $self->currently("Equipping a better weapon");
