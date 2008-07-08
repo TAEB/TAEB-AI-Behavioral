7ffc7b9925eba09b1e27378aeca15ac00162122f
bagofholding <bagofholding@gmail.com>
2008-07-08 11:34:01 -0500

    Add Chat action

---
+++ lib/TAEB/Action/Chat.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+package TAEB::Action::Chat;
+use TAEB::OO;
+extends 'TAEB::Action';
+with 'TAEB::Action::Role::Direction';
+
+use constant command => "#chat\n";
+
+has '+direction' => (
+    required => 1,
+);
+
+has 'amount' => (
+    traits   => [qw/TAEB::Provided/],
+    isa      => 'Int',
+    default  => 1,
+);
+
+sub respond_donate {
+    return shift->amount . "\n";
+    
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
