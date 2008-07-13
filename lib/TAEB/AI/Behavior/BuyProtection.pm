5ed1c841ce6a73057a32df70a6b0ca0c8ce41ac7
Shawn M Moore <sartak@gmail.com>
2008-07-13 11:48:15 -0500

    Some cleanup in BuyProtection

--- lib/TAEB/AI/Behavior/BuyProtection.pm
+++ lib/TAEB/AI/Behavior/BuyProtection.pm
@@ -6,10 +6,7 @@ use TAEB::Util ':colors';
 extends 'TAEB::AI::Behavior';
 
 sub can_buy_protection {
-
-    return unless TAEB->gold >= TAEB->level * 400;
-
-    return 1;
+    return TAEB->gold >= TAEB->level * 400;
 }
 
 sub prepare {
@@ -17,28 +14,32 @@ sub prepare {
 
     return 0 unless $self->can_buy_protection;
 
-    my $level = TAEB->nearest_level(sub { grep { $_->in_temple } shift->has_type('altar') })
-        or return 0;
+    my $level = TAEB->nearest_level(sub {
+        grep { $_->in_temple } shift->has_type('altar')
+    });
+
+    return 0 unless $level;
 
     if (TAEB->current_tile->type eq 'altar' && TAEB->current_tile->in_temple) {
-        my $found_monster;
+        my $found_priest;
         TAEB->each_adjacent(sub {
             my ($tile, $dir) = @_;
             if ($tile->has_monster && $tile->monster->is_priest) {
                 my $amount = TAEB->level * 400;
-                $self->do('Chat' => direction => $dir, 'amount' => $amount);
+                $self->do(chat => direction => $dir, 'amount' => $amount);
                 $self->currently("Chatting to a priest");
-                $found_monster = 1;
+                $found_priest = 1;
             }
         });
-        return 100 if $found_monster;
+        return 100 if $found_priest;
     }
 
     #find an altar to go to!
     my $path = TAEB::World::Path->first_match(
         sub {
             my $tile = shift;
-            return $tile->type eq 'altar' && $tile->in_temple },
+            $tile->type eq 'altar' && $tile->in_temple
+        },
         on_level => $level,
         why => "BuyProtection",
     );
