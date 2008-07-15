b422f2855af4b500697d459ca29519f5734ab7f2
Shawn M Moore <sartak@gmail.com>
2008-07-15 01:31:09 -0500

    Give opendoors a default type of opendoor

--- lib/TAEB/World/Tile/Opendoor.pm
+++ lib/TAEB/World/Tile/Opendoor.pm
@@ -3,6 +3,10 @@ package TAEB::World::Tile::Opendoor;
 use TAEB::OO;
 extends 'TAEB::World::Tile::Door';
 
+has '+type' => (
+    default => 'opendoor',
+);
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
