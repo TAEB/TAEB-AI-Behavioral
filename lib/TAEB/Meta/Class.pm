cd10ca6324d04bf34f63af1e8b2b8845da6b28f6
jluehrs2 <jluehrs2@uiuc.edu>
2008-07-19 18:52:47 -0500

    apply the AppInit and Subscription roles to the appropriate classes before make_immutable is called on those classes

--- lib/TAEB/Meta/Class.pm
+++ lib/TAEB/Meta/Class.pm
@@ -4,6 +4,8 @@ use Moose;
 extends 'Moose::Meta::Class';
 
 use TAEB::Meta::Attribute;
+use TAEB::Meta::Role::AppInit;
+use TAEB::Meta::Role::Subscription;
 
 around initialize => sub {
     my $orig = shift;
@@ -16,6 +18,13 @@ around initialize => sub {
         instance_metaclass  => 'Moose::Meta::Instance',
         @_,
     );
+};
+
+before make_immutable => sub {
+    my $self = shift;
+    $self->add_role(TAEB::Meta::Role::AppInit->meta);
+    $self->add_role(TAEB::Meta::Role::Subscription->meta)
+        if (any { /^(?:msg|exception|respond)_/ } $self->get_method_list);
 };
 
 no Moose;
