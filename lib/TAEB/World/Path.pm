cfb59acacdb25075efb6ca371857e288153cccb8
jluehrs2 <jluehrs2@uiuc.edu>
2008-07-19 13:55:25 -0500

    draw might not be defined in the config

--- lib/TAEB/World/Path.pm
+++ lib/TAEB/World/Path.pm
@@ -284,7 +284,7 @@ sub _dijkstra {
     my $include_endpoints = $args{include_endpoints};
     my $sokoban           = $from->known_branch
                          && $from->branch eq 'sokoban';
-    my $debug = TAEB->config->draw eq 'pathfind';
+    my $debug = (TAEB->config->draw || '') eq 'pathfind';
     my $start;
     if ($debug) {
         $args{from}->level->each_tile(sub {
