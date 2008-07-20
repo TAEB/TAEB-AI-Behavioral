0e4ce76307f196a8246fd2dc61c242e8c55bb933
jluehrs2 <jluehrs2@uiuc.edu>
2008-07-19 19:36:24 -0500

    apply_all_roles, not add_role

--- lib/TAEB/Meta/Class.pm
+++ lib/TAEB/Meta/Class.pm
@@ -23,8 +23,8 @@ around initialize => sub {
 
 before make_immutable => sub {
     my $self = shift;
-    $self->add_role(TAEB::Meta::Role::AppInit->meta);
-    $self->add_role(TAEB::Meta::Role::Subscription->meta)
+    Moose::Util::apply_all_roles($self, 'TAEB::Meta::Role::Initialize');
+    Moose::Util::apply_all_roles($self, 'TAEB::Meta::Role::Subscription')
         if (any { /^(?:msg|exception|respond)_/ } $self->get_method_list);
 };
 
