b6034a13ad209d48554a3c9bc4f02a60c73f28fa
jluehrs2 <jluehrs2@uiuc.edu>
2008-07-22 19:35:34 -0500

    also load the subscription role for classes that define send_message

--- lib/TAEB/Meta/Class.pm
+++ lib/TAEB/Meta/Class.pm
@@ -23,7 +23,8 @@ before make_immutable => sub {
     my $self = shift;
     Moose::Util::apply_all_roles($self, 'TAEB::Meta::Role::Initialize');
     Moose::Util::apply_all_roles($self, 'TAEB::Meta::Role::Subscription')
-        if (any { /^(?:msg|exception|respond)_/ } $self->get_method_list);
+        if (any { /^(?:msg|exception|respond)_/ || $_ eq 'send_message' }
+                $self->get_method_list);
 };
 
 no Moose;
