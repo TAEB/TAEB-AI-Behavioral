e4a1b070aa9f0ace62855538ec020bd2bd9eb879
Stefan O'Rear <stefanor@cox.net>
2008-11-12 02:52:59 -0500

    Fix last debug patch

--- lib/TAEB/Action/Move.pm
+++ lib/TAEB/Action/Move.pm
@@ -53,9 +53,9 @@ around new => sub {
         # XXX: this code is temporary. if the AI says to move, we move. since
         # our framework can't always cope with that, we have these workarounds
         my $monster = TAEB->current_level->at_direction($start)->monster;
-        TAEB->info("Trying to move into an uncooperative " . $monster->glyph .
-            "; Elberething instead.");
         if (defined $monster && $monster->can_move) {
+            TAEB->info("Trying to move into an uncooperative " .
+                $monster->glyph . "; Elberething instead.");
             if ($monster->respects_elbereth && TAEB->elbereth_count == 0) {
                 $action = 'Engrave';
             }
