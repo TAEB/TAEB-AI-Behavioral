ea31ace37d8cb9e4ecddfeca5a6b1e6ff4b782c4
Stefan O'Rear <stefanor@cox.net>
2008-11-13 01:52:08 -0500

    Add initial carrion T:W:I:Carrion

---
+++ lib/TAEB/World/Item/Carrion.pm
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+package TAEB::World::Item::Carrion;
+use TAEB::OO;
+use TAEB::Spoilers::Monster;
+extends 'TAEB::World::Food';
+
+has '+class' => (
+    default => 'food',
+);
+
+has is_forced_verboten => (
+    isa     => 'Bool',
+    default => 0,
+);
+
+has estimated_date => (
+    isa     => 'Bool',
+    default => 0,
+);
+
+sub estimate_age { TAEB->turn - shift->estimated_date; }
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
