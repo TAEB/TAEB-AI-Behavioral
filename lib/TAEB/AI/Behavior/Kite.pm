90048608fd8427b7e846d8fc83e14d87dc3e12a3
jluehrs2 <jluehrs2@uiuc.edu>
2008-11-16 23:14:32 -0500

    remove kite behavior debug spam

--- lib/TAEB/AI/Behavior/Kite.pm
+++ lib/TAEB/AI/Behavior/Kite.pm
@@ -13,24 +13,19 @@ extends 'TAEB::AI::Behavior';
 sub prepare {
     my $self = shift;
 
-    TAEB->debug("considering kite...");
 
     my @enemies = grep { $_->in_los } TAEB->current_level->has_enemies;
 
-    TAEB->debug ((scalar @enemies) . " enemies visible...");
     # For now, only handle one-on-one fights
     return 0 unless @enemies == 1;
 
     my $enemy = $enemies[0];
 
-    TAEB->debug($enemy->y . "," . $enemy->x . " " . TAEB->y . "," . TAEB->x);
     # and unless the enemy is next to us and kitable, act normally
     return 0 unless abs($enemy->x - TAEB->x) <= 1
                  && abs($enemy->y - TAEB->y) <= 1;
 
-    TAEB->debug("and he's next to us...");
     return 0 unless $enemy->can_be_outrun && TAEB->senses->can_move;
-    TAEB->debug("and we can outrun him...");
     #return 0 unless $enemy->should_attack_at_range;
 
     # Don't try to kite non-infravisible monsters in the dark.  TAEB
@@ -41,7 +36,6 @@ sub prepare {
     # do we have a projectile to throw?  No sense backing away otherwise (yet)
     return 0 unless defined (TAEB->inventory->has_projectile) &&
         !$enemy->tile->in_shop;
-    TAEB->debug("and we have projectiles...");
 
     # Until EkimFight is the default, all this does more harm than good,
     # because TAEB will just walk up to the monster
@@ -83,13 +77,11 @@ sub prepare {
     return 0 if (TAEB->current_tile->type eq 'opendoor' ||
         $to->type eq 'opendoor') && $back =~ /[yubn]/;
 
-    TAEB->debug("We have a useful move! (" . $back . ")");
 
     $self->do(move =>
         direction   => $back,
     );
     $self->currently("Kiting.");
-    TAEB->debug("and... " . $self->action);
     return 100;
 }
 
