0e7374bfbaf7cd7a720223fbde85a90cc1b48acc
jluehrs2 <jluehrs2@uiuc.edu>
2008-11-16 21:22:59 -0500

    update Explorer personality with the new interface

--- lib/TAEB/AI/Personality/Explorer.pm
+++ lib/TAEB/AI/Personality/Explorer.pm
@@ -12,32 +12,32 @@ TAEB::AI::Personality::Explore - descend only after exploring the level
 sub weight_behaviors {
     my $fight = TAEB->config->fight_behavior || 'Melee';
 
-    return {
-        FixHunger          => 1_000_000,
-        Heal               => 750_000,
-        FixStatus          => 700_000,
-        Defend             => 400_000,
-        AttackSpell        => 175_000,
-        BuffSelf           => 170_000,
-        Kite               => 100_000,
-        $fight             => 80_000,
-        Projectiles        => 49_000,
-        Vault              => 48_000,
-        Shop               => 47_000,
-        Carrion            => 45_000,
-        GetItems           => 41_000,
-        Identify           => 24_750,
-        DipForExcalibur    => 24_000,
-        Wish               => 23_000,
-        BuyProtection      => 21_000,
-        Doors              => 10_000,
-        DeadEnd            => 5_000,
-        Explore            => 2_500,
-        CurseCheck         => 2_250,
-        Descend            => 1_000,
-        Search             => 300,
-        RandomWalk         => 1,
-    };
+    return (
+        "FixHunger",
+        "Heal",
+        "FixStatus",
+        "Defend",
+        "AttackSpell",
+        "BuffSelf",
+        "Kite",
+        "$fight",
+        "Projectiles",
+        "Vault",
+        "Shop",
+        "Carrion",
+        "GetItems",
+        "Identify",
+        "DipForExcalibur",
+        "Wish",
+        "BuyProtection",
+        "Doors",
+        "DeadEnd",
+        "Explore",
+        "CurseCheck",
+        "Descend",
+        "Search",
+        "RandomWalk",
+    );
 }
 
 __PACKAGE__->meta->make_immutable;
