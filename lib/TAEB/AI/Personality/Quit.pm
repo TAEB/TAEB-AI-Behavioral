071e46b8e4348bf9e7d3195f0e801cd8499bf223
jluehrs2 <jluehrs2@uiuc.edu>
2008-11-18 22:03:40 -0500

    next_action actually has to return an action

--- lib/TAEB/AI/Personality/Quit.pm
+++ lib/TAEB/AI/Personality/Quit.pm
@@ -9,7 +9,7 @@ TAEB::AI::Personality::Quit - I just can't take it any more...
 
 =cut
 
-sub next_action { "#quit\ny" }
+sub next_action { TAEB::Action->new_action(custom => string => "#quit\ny") }
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
