346103e2a603d5d654ca4696d033182748d91e5c
Stefan O'Rear <stefanor@cox.net>
2008-11-19 03:23:51 -0500

    add a pathfinder which uses approximately the same algorith as NetHack (someday this will be a module with a testsuite)

--- lib/TAEB/World/Level.pm
+++ lib/TAEB/World/Level.pm
@@ -243,6 +243,79 @@ sub radiate {
 
 }
 
+# not handled: swimming, phasing
+# possibly buggy: everything
+sub calculate_fov {
+    my $self   = shift;
+    my $source = shift || $self->at;
+
+    # If you aren't on the map, you can't see a thing
+    if (!defined ($source)) { return [] }
+
+    my $startx = $source->x;
+    my $starty = $source->y;
+
+    my @visible;
+    $visible[$startx][$starty] = 1;
+
+    my $clear = sub {
+        my ($x, $y) = @_;
+
+        my $tile = $self->at($x, $y);
+
+        defined $tile && $tile->is_transparent;
+    };
+
+    for my $octant (0 .. 7) {
+        my $maj_dx = (1, 0, 0, -1, -1, 0, 0, 1)[$octant];
+        my $maj_dy = (0, -1, -1, 0, 0, 1, 1, 0)[$octant];
+        my $min_dx = (0, 1, -1, 0, 0, -1, 1, 0)[$octant];
+        my $min_dy = (-1, 0, 0, -1, 1, 0, 0, 1)[$octant];
+
+        my $shadowcaster;
+        $shadowcaster = sub {
+            my ($dist, $min_slope, $max_slope) = @_;
+
+            my $min_minor = int($min_slope * $dist + 0.5);
+            my $max_minor = int($max_slope * $dist + 0.5);
+
+            my $last_clear = 0;
+            my $window_start = $min_minor;
+
+            for my $minor ($min_minor .. $max_minor) {
+                my $start = ($minor - 0.5) / $dist;
+                my $end   = ($minor + 0.5) / $dist;
+
+                $start = $min_slope if $start < $min_slope;
+                $end   = $max_slope if $end   > $max_slope;
+
+                my $x = $startx + $maj_dx * $dist + $min_dx * $minor;
+                my $y = $starty + $maj_dy * $dist + $min_dy * $minor;
+
+                $visible[$x][$y] = 1;
+
+                if ($clear->($x,$y) xor $last_clear) {
+                    if ($last_clear) {
+                        $last_clear = 0;
+                        $shadowcaster->($dist+1, $window_start, $start);
+                    } else {
+                        $last_clear = 1;
+                        $window_start = $start;
+                    }
+                }
+            }
+
+            if ($last_clear) {
+                $shadowcaster->($dist+1, $window_start, $max_slope)
+            }
+        };
+
+        $shadowcaster->(1, 0, 1);
+    }
+
+    return \@visible;
+}
+
 sub remove_monster {
     my $self    = shift;
     my $monster = shift;
