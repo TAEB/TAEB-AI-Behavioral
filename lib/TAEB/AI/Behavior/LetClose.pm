b925838e5f8d6e072298426e85678112acb55a0d
Stefan O'Rear <stefanor@cox.net>
2008-11-20 03:46:46 -0500

    tweak: let close at distance 3

--- lib/TAEB/AI/Behavior/LetClose.pm
+++ lib/TAEB/AI/Behavior/LetClose.pm
@@ -10,7 +10,7 @@ sub prepare {
     my @enemies = grep { $_->in_los } TAEB->current_level->has_enemies;
 
     my @beckon = grep { $_->will_chase && $_->distance >= 2
-        && $_->is_meleeable && $_->distance < 9} @enemies;
+        && $_->is_meleeable && $_->distance < 4} @enemies;
 
     if (@beckon) {
         $self->write_elbereth;  # this gem taken from EkimFight.  Why not?
