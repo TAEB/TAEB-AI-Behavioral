a52c2851d04736a673b7b6c23e7b82af0c3f79b6
Stefan O'Rear <stefanor@cox.net>
2008-11-20 02:02:04 -0500

    Add MakeNoise action

---
+++ lib/TAEB/AI/Behavior/MakeNoise.pm
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+package TAEB::AI::Behavior::MakeNoise;
+use TAEB::OO;
+extends 'TAEB::AI::Behavior';
+
+sub prepare {
+    my $self = shift;
+
+    return if !TAEB->can_kick;
+
+    my @enemies = grep { $_->in_los } TAEB->current_level->has_enemies;
+
+    my @beckon = grep { $_->is_hostile && $_->would_chase &&
+        $_->probably_sleeping && $_->distance2 < 20 * TAEB->xl } @enemies;
+
+    return if !@beckon;
+
+    TAEB->current_tile->apply_adjacent(sub {
+        my ($tile, $dir) = @_;
+
+        return if $tile->item_count;
+        return if $tile->has_monster;
+        return if $tile->type =~ /throne|altar|tree|fountain|sink|closeddoor/;
+        return if $tile->type =~ /wall|rock|grave|ironbars|stairs/ &&
+            TAEB->hp < 50;
+
+        $self->currently("Kicking a " . $tile->type . " to wake up a " .
+            ($beckon[0]->spoiler || { name => "monster" })->name . ".");
+        $self->urgency('normal');
+        $self->action(kick => direction => $dir);
+    });
+}
+
+sub urgencies {
+    return {
+        normal => "Kicking something to make noise",
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
