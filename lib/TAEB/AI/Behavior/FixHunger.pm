34f8248c2118f58754eff88eb1ca6f5ce26f386c
Stefan O'Rear <stefanor@cox.net>
2008-11-20 22:53:51 -0500

    Throttle TAEB's tendency to hoard food

--- lib/TAEB/AI/Behavior/FixHunger.pm
+++ lib/TAEB/AI/Behavior/FixHunger.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package TAEB::AI::Behavior::FixHunger;
 use TAEB::OO;
+use List::Util 'sum';
 extends 'TAEB::AI::Behavior';
 
 sub prepare {
@@ -16,7 +17,16 @@ sub prepare {
     }
 
     if (TAEB->nutrition < 200 && TAEB::Action::Eat->any_food) {
-        $self->do(eat => item => 'any');
+        my ($item, $prio) = (undef, -1000);
+
+        for my $meal (TAEB->inventory->find(\&good_food)) {
+            my $p = $meal->weight / $meal->nutrition;
+
+            $p -= 1000 if $meal->identity =~ qr/sprig|carrot|leaf|lump/;
+
+            ($item,$prio) = ($meal,$p) if $prio < $p;
+        }
+        $self->do(eat => item => $item);
         $self->currently("Eating food.");
         $self->urgency('important');
         return;
@@ -30,11 +40,32 @@ sub urgencies {
     },
 }
 
+sub good_food {
+    my $item = shift;
+    my $great = shift;
+
+    return 0 unless $item->match(weight => sub { shift() < 100 });
+    return 0 unless TAEB::Spoilers::Item::Food->should_eat($item);
+    return 0 if $great && $item->weight > (40 * $item->nutrition);
+
+    return 1;
+}
+
 sub pickup {
     my $self = shift;
     my $item = shift;
-    return 0 unless $item->match(weight => sub { shift() < 100 });
-    return TAEB::Spoilers::Item::Food->should_eat($item);
+
+    return 0 if !good_food($item);
+
+    my $good = sum 0, map { $_->quantity * $_->nutrition }
+        TAEB->find_item(sub{ good_food($_,1); });
+    my $food = sum 0, map { $_->quantity * $_->nutrition }
+        TAEB->find_item(\&good_food);
+
+    return 0 if $food >= 3000;
+    return 0 if $good >= 1500 && !good_food($item,1);
+
+    return 1;
 }
 
 __PACKAGE__->meta->make_immutable;
