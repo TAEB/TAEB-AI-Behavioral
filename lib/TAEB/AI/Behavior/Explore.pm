ec00af9f8ec246f59b3677e0c46aa2f726092736
jluehrs2 <jluehrs2@uiuc.edu>
2008-11-23 02:34:10 -0500

    oops, don't mask the variable definition here

--- lib/TAEB/AI/Behavior/Explore.pm
+++ lib/TAEB/AI/Behavior/Explore.pm
@@ -42,7 +42,7 @@ sub prepare {
         return 0 if $level->branch ne TAEB->current_level->branch;
         return not $level->fully_explored;
     });
-    my $level = TAEB->shallowest_level(sub { not shift->fully_explored })
+    $level = TAEB->shallowest_level(sub { not shift->fully_explored })
         if !$level;
 
     return if !$level;
