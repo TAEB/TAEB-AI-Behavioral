599e032a54a807c3dd1ec796932cf810330d9f74
jluehrs2 <jluehrs2@uiuc.edu>
2008-11-23 01:02:21 -0500

    detect trap types using farlook, if necessary

--- lib/TAEB/World/Tile/Trap.pm
+++ lib/TAEB/World/Tile/Trap.pm
@@ -5,10 +5,31 @@ extends 'TAEB::World::Tile';
 use TAEB::Util ':colors';
 
 has trap_type => (
-    isa => 'Str', # this should become an enum
+    isa => 'TAEB::Type::Trap',
 );
 
 augment debug_color => sub { Curses::A_BOLD | Curses::COLOR_PAIR(COLOR_BLUE) };
+
+sub reblessed {
+    my $self = shift;
+
+    my $trap_type = $TAEB::Util::trap_colors{$self->color};
+    if (ref $trap_type) {
+        TAEB->enqueue_message(check => tile => $self);
+    }
+    else {
+        $self->trap_type($trap_type);
+    }
+}
+
+sub farlooked {
+    my $self = shift;
+    my $msg  = shift;
+
+    if ($msg =~ /trap.*\((.*?)\)/) {
+        $self->trap_type($1);
+    }
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
