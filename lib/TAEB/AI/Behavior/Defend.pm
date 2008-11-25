5d15a4afc5c662364fd84c8cbcc27a7b1645aa1e
Alex Smith <ais523@bham.ac.uk>
2008-11-24 20:57:04 -0500

    Burn Elbereth in an emergency, if we can

--- lib/TAEB/AI/Behavior/Defend.pm
+++ lib/TAEB/AI/Behavior/Defend.pm
@@ -10,6 +10,8 @@ sub prepare {
     if (TAEB->hp * 2 <= TAEB->maxhp) {
         my $can_engrave = TAEB->can_engrave;
         my $elbereths   = lazy { TAEB->elbereth_count };
+	my $burned      = $elbereths >= 1
+	    && TAEB->current_tile->engraving_type eq 'burned';
 
         my ($adjacent_ignoring, $adjacent_respecting) = (0, 0);
         TAEB->each_adjacent(sub {
@@ -20,6 +22,18 @@ sub prepare {
                 ? ++$adjacent_respecting
                 : ++$adjacent_ignoring
         });
+
+	# if at least 3 adjacent monsters obey Elbereth, and we can burn an
+	# Elbereth but haven't, burn one (so as not to get surrounded).
+	if ($adjacent_respecting >= 3 && $can_engrave && !$burned &&
+	    (TAEB->find_item('wand of fire') ||
+	     TAEB->find_item('wand of lightning'))) {
+	    $self->write_elbereth(add_engraving => 0,
+				  method => 'best');
+	    $self->currently("burning Elbereth because I'm surrounded");
+	    $self->urgency('normal');
+	    return;
+	}
 
         # if there's an adjacent monster that ignores Elbereth, then we only
         # write Elbereth if there's no Elbereth on the ground AND there's an
@@ -32,9 +46,9 @@ sub prepare {
             return;
         }
 
-        # otherwise, we write Elbereth if we can and there's not already an
-        # excessive amount of them
-        if ($can_engrave && $elbereths < 3) {
+        # otherwise, we write Elbereth if we can, there's not already an
+        # excessive amount of them, and we don't have a permanent E
+        if ($can_engrave && $elbereths < 3 && !$burned) {
             $self->write_elbereth(add_engraving => $elbereths ? 1 : 0);
             $self->urgency('normal');
             return;
@@ -60,6 +74,24 @@ sub prepare {
         $self->urgency('unimportant');
         return;
     }
+}
+
+sub pickup {
+    my $self = shift;
+    my $item = shift;
+    return 1 if ($item->match(identity => 'wand of fire') ||
+		 $item->match(identity => 'wand of lightning'))
+	     && (!$item->known_charges || $item->charges > 0);
+    return 0;
+}
+
+sub drop {
+    my $self = shift;
+    my $item = shift;
+    return 1 if ($item->match(identity => 'wand of fire') ||
+		 $item->match(identity => 'wand of lightning'))
+	     && ($item->known_charges && $item->charges == 0);
+    return undef;
 }
 
 sub urgencies {
