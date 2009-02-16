6a2a107736029a878b7d547c731719bcdbbf65b3
Shawn M Moore <sartak@gmail.com>
2009-02-15 21:45:10 -0500

    First stab at real armor handling

--- lib/TAEB/AI/Behavioral/Behavior/Equip.pm
+++ lib/TAEB/AI/Behavioral/Behavior/Equip.pm
@@ -3,31 +3,53 @@ use TAEB::OO;
 use TAEB::Spoilers::Combat;
 extends 'TAEB::AI::Behavioral::Behavior';
 
+sub _rate_armor {
+    my $self = shift;
+    my $item = shift;
+
+    return 0 if !$item;
+
+    return ($item->ac || 0) + ($item->enchantment || 0);
+}
+
 sub prepare {
     my $self = shift;
-    my $current_weapon = TAEB->inventory->wielded || '-';
-    my $current_avgdam = TAEB::Spoilers::Combat::damage($current_weapon);
 
-    my ($best_weapon, $best_avgdam) = ($current_weapon, $current_avgdam);
-    TAEB->inventory->each(sub {
-        return if $_->match(buc => ['cursed', undef]);
-        my $avgdam = TAEB::Spoilers::Combat::damage($_);
-        if ($avgdam > $best_avgdam) {
-            $best_avgdam = $avgdam;
-            $best_weapon = $_;
+    return if $self->prepare_armor;
+}
+
+sub prepare_armor {
+    my $self = shift;
+
+    for my $slot (TAEB->equipment->armor_slots) {
+        my $incumbent        = TAEB->equipment->$slot;
+        my $incumbent_rating = $self->_rate_armor($incumbent);
+
+        my @candidates = TAEB->inventory->find(
+            type    => 'armor',
+            subtype => $slot,
+        );
+
+        for my $candidate (@candidates) {
+            my $rating = $self->_rate_armor($candidate);
+            next if $rating < $incumbent_rating;
+
+            if ($incumbent) {
+                $self->do(unwear => item => $incumbent);
+                $self->currently("Removing $incumbent to wear $candidate.");
+            }
+            else {
+                $self->do(wear => item => $candidate);
+                $self->currently("Putting on $candidate.");
+            }
+
+            $self->urgency('normal');
+
+            return $candidate;
         }
-    }, class => 'weapon');
-    # XXX: need to handle switching from barehanded to weapon and back
-    if (blessed $best_weapon) {
-        return if $best_weapon->match(slot => $current_weapon->slot);
-    }
-    else {
-        return if $best_weapon eq $current_weapon;
     }
 
-    $self->do(wield => weapon => $best_weapon);
-    $self->currently("Equipping a better weapon");
-    $self->urgency('unimportant');
+    return 0;
 }
 
 __PACKAGE__->meta->make_immutable;
