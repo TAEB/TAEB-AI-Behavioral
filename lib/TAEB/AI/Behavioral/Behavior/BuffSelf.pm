adcb46c7bd1d9ea13776e2f5263c509c453e9e21
Stefan O'Rear <stefanor@cox.net>
2009-02-18 23:59:54 -0500

    New, still somewhat sketchy, BuffSelf

--- lib/TAEB/AI/Behavioral/Behavior/BuffSelf.pm
+++ lib/TAEB/AI/Behavioral/Behavior/BuffSelf.pm
@@ -2,48 +2,117 @@ package TAEB::AI::Behavioral::Behavior::BuffSelf;
 use TAEB::OO;
 extends 'TAEB::AI::Behavioral::Behavior';
 
-sub use_spells { ('protection', 'haste self') }
+# The basic premise of the new BuffSelf is "cast the best buff that
+# you can afford to improve".  In combat we can afford anything if
+# we have it, out of combat we can only afford things which cost
+# 1/3 or less of our mp regen ... cumulative with lower priority
+# things.
 
-has buff_level => (
-    is      => 'ro',
-    isa     => 'HashRef[Int]',
-    default => sub { {} },
+sub spell {
+    my ($spell, $dur) = @_;
+
+    return unless defined ($spell = TAEB->find_castable($spell));
+
+    { action => [ cast => spell => $spell ], cost => $spell->power,
+        dur => $dur }
+}
+
+sub potion {
+    my ($name, $dur, $durb) = @_;
+
+    my $pot = TAEB->has_item(identity => "potion of " . $name, is_blessed => 1)
+           || TAEB->has_item(identity => "potion of " . $name);
+
+    return unless $pot;
+
+    { action => [ quaff => item => $pot ], cost => 1000,
+        dur => $pot->is_blessed ? $durb : $dur }
+}
+
+has buff_options => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    auto_deref => 1,
+    default => sub { [
+        {
+            buff => "very fast",
+            have => sub { TAEB->senses->is_very_fast },
+            impl => sub {
+                spell ("haste self", 60) || # XXX 90 if Skilled in escape
+                potion("speed", 60, 90)
+            }
+        },
+        # XXX galloping - only useful with riding + kiting
+        # these require the framework to have a notion of see invisible
+        #{
+        #    buff => "invisible",
+        #    have => sub { TAEB->senses->invisible },
+        #    impl => sub {
+        #        spell ("invisibility", 38) ||
+        #        potion("invisibility", 38, 10e9999)
+        #    }
+        #},
+        #{
+        #    buff => "see invisible",
+        #    have => sub { TAEB->senses->see_invisible },
+        #    impl => sub {
+        #        potion("see invisible", 800, 10e9999)
+        #    }
+        #},
+        {
+            buff => "protection",
+            levels => 1,
+            have => sub { TAEB->senses->spell_protection },
+            impl => sub {
+                my $will_get = TAEB->senses->spell_protection_return;
+
+                return if !$will_get;
+
+                spell("protection", $will_get * 10) # XXX 20 if Expert
+                # also the value of protecting varies a bit
+            }
+        },
+        # XXX levitation - sometimes harmful
+        # XXX detect monsters - would probably confuse TAEB too much
+    ]},
 );
 
 sub prepare {
     my $self = shift;
-    my ($max_spell, $max_urgency) = (undef, 0);
 
+    # TODO actually implement permabuffing
+    #
+    # Interesting case: suppose we have Protection and Haste Self, but only
+    # enough MP regen to keep one of them active.  We pick Haste Self.  Now
+    # we put on speed boots; they don't identify since we are Very_fast, and
+    # it seems exactly as if Haste Self never wore off.  How do we know to
+    # start Protecting?
     return unless TAEB->current_level->has_enemies;
 
-    for ($self->use_spells) {
-        my $spell = TAEB->find_castable($_)
-            or next;
-        my $urgency = $self->buff_level->{$spell->name} ? 'fallback'
-                                                        : 'unimportant';
+    for my $buff ($self->buff_options) {
+        next if $buff->{have}->() && !$buff->{levels};
 
-        ($max_spell, $max_urgency) = ($spell, $urgency)
-            if $self->numeric_urgency($urgency) >
-               $self->numeric_urgency($max_urgency);
+        next unless (my $imp = $buff->{impl}->());
+
+        $self->do(@{$imp->{action}});
+        $self->currently("Buffing $buff->{buff}");
+        $self->urgency('normal');
+        return;
     }
-
-    return if $max_urgency == 0;
-
-    $self->do(cast => spell => $max_spell);
-    $self->currently("Casting ".($max_spell->name).".");
-    $self->urgency($max_urgency);
 }
 
-sub msg_buff {
-    my $self      = shift;
-    my $spellname = shift;
-    my $is_buffed = shift;
-    my $delta     = shift;
+sub pickup {
+    my $self = shift;
+    my $item = shift;
 
-    $self->buff_level->{$spellname} += $delta;
-    if ($self->buff_level->{$spellname} xor $is_buffed) {
-        $self->buff_level->{$spellname} = $is_buffed;
-    }
+    # XXX find a better way to handle this
+    # we need more information about the buffs than FixStatus uses...
+
+    return 1 if $item->identity eq 'potion of invisibility';
+    return 1 if $item->identity eq 'potion of speed';
+    return 1 if $item->identity eq 'potion of see invisible';
+
+    return;
 }
 
 __PACKAGE__->meta->make_immutable;
