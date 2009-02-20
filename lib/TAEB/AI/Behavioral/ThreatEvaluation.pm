f48b85f4b3756fb760fe4c2f4cff5400540d7c6d
Stefan O'Rear <stefanor@cox.net>
2009-02-20 02:49:12 -0500

    Add a class for ThreatEvaluations

---
+++ lib/TAEB/AI/Behavioral/ThreatEvaluation.pm
@@ -0,0 +1,40 @@
+package TAEB::AI::Behavioral::ThreatEvaluation;
+use TAEB::OO;
+
+# Data class for evaluations of monster threat levels, since it's more
+# typo-resistant than a simple hashref.  Monster threat levels depend
+# on terrain and buffs, and need to be re-evaluated every turn.  To get
+# an evaluation for a monster, ask your friendly local TAEB->ai.
+
+# Is this monster worth spending major resources (consumables and wand
+# charges) on?
+
+has spend_major => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+# Is this monster worth spending minor resources (mana and mulchable
+# ammunition) on?
+
+has spend_minor => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+# Is this a monster that we want to avoid melee contact with at
+# almost any cost?
+
+has avoid_melee => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 0,
+);
+
+__PACKAGE__->meta->make_immutable;
+no TAEB::OO;
+
+1;
+
