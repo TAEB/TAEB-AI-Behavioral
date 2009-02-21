24a9cb35fc587d0d2885be07dbe6ac35ae613a0a
Shawn M Moore <sartak@gmail.com>
2009-02-20 23:45:42 -0500

    Syntax error fix

--- lib/TAEB/AI/Behavioral/Behavior/Escape.pm
+++ lib/TAEB/AI/Behavioral/Behavior/Escape.pm
@@ -3,7 +3,7 @@ use TAEB::OO;
 extends 'TAEB::AI::Behavioral::Behavior::GotoTile';
 
 sub first_pass {
-    return unless = TAEB->current_level->has_type('stairsup');
+    return unless TAEB->current_level->has_type('stairsup');
     return if TAEB->senses->is_confused || TAEB->senses->is_stunned;
 }
 
