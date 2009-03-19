1af507b23bd7f3b4a51d259974e500a4cc57e35c
Shawn M Moore <sartak@gmail.com>
2009-03-19 18:05:26 -0500

    Throw a useful error when Behavioral is used directly

--- lib/TAEB/AI/Behavioral.pm
+++ lib/TAEB/AI/Behavioral.pm
@@ -4,5 +4,7 @@ use warnings;
 
 our $VERSION = '0.01';
 
+sub new { die "Do not use TAEB::AI::Behavioral directly. Use a Personality subclass, such as TAEB::AI::Behavioral::Personality::Explorer." }
+
 1;
 
