415982ec3f14ae7c69c23426552cb6d57192d1e9
Jesse Luehrs <doy@tozt.net>
2013-06-26 20:26:46 -0400

    fix this logic

--- lib/TAEB/AI/Behavioral/Behavior/Light.pm
+++ lib/TAEB/AI/Behavioral/Behavior/Light.pm
@@ -1,15 +1,14 @@
 package TAEB::AI::Behavioral::Behavior::Light;
 use Moose;
 use TAEB::OO;
-use TAEB::Util 'any';
 extends 'TAEB::AI::Behavioral::Behavior';
 
 sub prepare {
     my $self = shift;
 
     my @lamps = TAEB->inventory->find(identity => qr/lamp|lantern/);
+    @lamps = grep { !$_->is_lit && !$_->cost } @lamps;
     return unless @lamps;
-    return if any { $_->is_lit || $_->cost } @lamps;
 
     $self->do(apply => item => $lamps[0]);
     $self->currently("Lighting up a lamp");
