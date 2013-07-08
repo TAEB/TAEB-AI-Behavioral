515dbd913a56cdbcedfc692a3f0ef67d856bef2c
Jesse Luehrs <doy@tozt.net>
2013-07-08 01:46:57 -0400

    fix the Vault behavior

--- lib/TAEB/AI/Behavioral/Behavior/Vault.pm
+++ lib/TAEB/AI/Behavioral/Behavior/Vault.pm
@@ -9,20 +9,27 @@ sub prepare {
     if (TAEB->gold > 0 && $self->drop(TAEB->new_item('1 gold piece'))) {
         $self->do('drop', items => [ TAEB->inventory->find('gold piece') ]);
         $self->currently("Dropping my gold");
-        $self->urgency('normal');
+        $self->urgency('important');
         return;
     }
 
-    return if TAEB->any_adjacent(sub {
+    my $vault_guard = TAEB->any_adjacent(sub {
         my $monster = shift->monster;
         return defined $monster && $monster->is_vault_guard;
     });
+    if ($vault_guard) {
+        $self->do('search', iterations => 1);
+        $self->currently("Waiting for the vault guard to move");
+        $self->urgency('important');
+        return;
+    }
 
     if (TAEB->following_vault_guard) {
+        TAEB->log->vault("Trying to follow a vault guard");
         my $path = TAEB::World::Path->first_match(sub {
             my $monster = shift->monster;
             return defined $monster && $monster->is_vault_guard;
-        });
+        }, include_endpoints => 1);
 
         return $self->if_path(
             $path,
